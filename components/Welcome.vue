2e9d22f3c8986a06f04d315d178146d4044083af
LaShawn <lts001@gmail.com>
2022-01-30 01:34:19 +0900

    add cta button

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="p-4 bg-gray-100 h-full">
-    <h1>Welcome to Find a Doc</h1>
+    <span class="text-4xl">
+      The open-source project that's helping Japan get vaccinated
+    </span>
+    <span>
+      <a href="https://info.findadoc.jp">
+        <button
+          type="button"
+          class="
+            inline-flex
+            items-center
+            px-6
+            py-3
+            border border-transparent
+            text-base
+            font-medium
+            rounded-md
+            shadow-sm
+            text-white
+            bg-indigo-600
+            hover:bg-indigo-700
+            focus:outline-none
+            focus:ring-2
+            focus:ring-offset-2
+            focus:ring-indigo-500
+          "
+        >
+          Get Booster Shot Info
+        </button>
+      </a>
+    </span>
   </div>
 </template>
