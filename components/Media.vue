eb3c59c4a533976a4b2109d9127785a43cf9242f
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2022-01-30 19:11:14 +0900

    Create Homepage (#8)
    Init Nuxt 3 and create new homepage

---
+++ components/Media.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="h-full m-12 text-gray-700">
+    <div>
+      <span class="text-4xl font-semibold">Find a Doc in the News</span>
+    </div>
+    <div class="flex flex-col mt-4 opacity-80">
+      <div class="flex justify-center m-4">
+        <a
+          href="https://www.japantimes.co.jp/community/2021/06/28/our-lives/lashawn-toyoda-findadocjp/"
+        >
+          <img
+            src="~/assets/images/homepage/japantimes-logo.png"
+            alt="the japan times newspaper logo"
+          />
+        </a>
+      </div>
+      <div class="flex justify-center m-4">
+        <a
+          href="https://www.reuters.com/world/asia-pacific/tokyo-mom-creates-covid-19-vaccination-database-amid-information-vacuum-2021-07-01/"
+        >
+          <img
+            src="~/assets/images/homepage/reuters-logo.png"
+            alt="reuters newspaper logo"
+          />
+        </a>
+      </div>
+      <div class="flex justify-center m-4">
+        <a
+          href="https://mainichi.jp/english/articles/20211001/p2a/00m/0na/023000c"
+        >
+          <img
+            src="~/assets/images/homepage/mainichi-logo.png"
+            alt="mainichi shimbun logo"
+          />
+        </a>
+      </div>
+      <div class="flex justify-center mt-8 text-gray-700 font-semibold">
+        <a href="https://www.netlify.com/">Deployed on Netlify</a>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-full">
+    <TopNav />
+    <div class="flex flex-col md:flex-row h-full">
+      <Welcome />
+      <FeaturedNews />
+    </div>
+    <div class="w-full">
+      <Media />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+});
+</script>
