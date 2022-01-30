8f8c48cc7b1b7af57967b4679381479532a4c82d
LaShawn <lts001@gmail.com>
2022-01-30 21:01:54 +0900

    add donate button

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -1,3 +1,16 @@
 <template>
-  <div class="p-4 border-b font-semibold text-xl text-gray-800">Find a Doc</div>
+  <div class="p-4 border-b flex justify-between">
+    <div class="font-semibold text-xl text-gray-800">Find a Doc</div>
+    <div>
+      <a href="https://ko-fi.com/Z8Z14YNUR" target="_blank"
+        ><img
+          height="36"
+          style="border: 0px; height: 36px"
+          src="https://cdn.ko-fi.com/cdn/kofi1.png?v=3"
+          border="0"
+          alt="Buy Me a Coffee at ko-fi.com"
+      /></a>
+    </div>
+  </div>
 </template>
+
