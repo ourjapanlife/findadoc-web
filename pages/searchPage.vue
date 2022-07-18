db2dd25d269f6cebb8ac1c3a8aa94f39006d91fd
Philip Ermish <philipermish@gmail.com>
2022-07-18 22:50:45 +0900

    adding pinia

--- pages/searchPage.vue
+++ pages/searchPage.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="h-full">
-    <TopNav />
-    <div class="flex flex-col md:flex-row h-full">
-      <Welcome />
-      <FeaturedNews />
+    <div class="h-full">
+        <TopNav />
+        <div class="flex flex-col md:flex-row h-full">
+            <Welcome />
+            <FeaturedNews />
+        </div>
+        <div class="w-full">
+            <Media />
+        </div>
     </div>
-    <div class="w-full">
-      <Media />
-    </div>
-  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
     props: {
-        msg: {
-            type: String,
-            required: true
-        }
+        name: String,
+        msg: { type: String, required: true }
     },
     data() {
         return {
-            name: 'LogRocket',
+            count: 1
         }
+    },
+    mounted() {
     }
 })
 </script>
