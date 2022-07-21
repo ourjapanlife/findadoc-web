f5b33a8755002a69529abc945ee4bea8fe43b2e7
Philip Ermish <philipermish@gmail.com>
2022-07-21 22:26:23 +0900

    removing .nuxt folder from git

--- nuxt.config.js
+++ nuxt.config.js
@@ -91,25 +91,19 @@ export default defineNuxtConfig({
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [],
 
-    // Auto import components: https://go.nuxtjs.dev/config-components
-    components: true,
-
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
     // https://go.nuxtjs.dev/typescript
         '@nuxt/typescript-build',
         // https://go.nuxtjs.dev/tailwindcss
         '@nuxtjs/tailwindcss',
-        '@nuxtjs/google-analytics',
+        // '@nuxtjs/google-analytics',
     ],
 
     // Modules: https://go.nuxtjs.dev/config-modules
     modules: [],
-
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {},
     target: 'static',
-    googleAnalytics: {
-        id: 'UA-186921459-1',
-    },
+    // googleAnalytics: {
+    //     id: 'UA-186921459-1',
+    // },
 })
