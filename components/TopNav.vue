6c43c653bee3d2d347d540ab6cb1c2655d9fb747
Sulaymane Sillah <sssillah1@gmail.com>
2022-10-18 21:24:58 +0900

    added hamburger icon issue 54

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -18,6 +18,9 @@
         </div>
         <div id="searchbar" class="align-middle">
             <SearchBar />
+            <div class="cursor-pointer md:hidden">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            </div>
         </div>
         <nav class="flex gap-4 mx-6 absolute right-20">
             <NuxtLink to="/about" class="px-2 mx-auto">
@@ -28,3 +31,22 @@
         <LocaleSelector />
     </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+   
+    props: {
+        msg: {
+            type: String,
+            required: false
+        }
+    },
+    data() {
+        return {
+            name: '',
+        }
+    }
+})
+</script>
