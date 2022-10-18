f3b52df8f6e156ed45c8de612beea4151f1e5e29
Ann Kilzer キルザー杏 <akilzer@gmail.com>
2022-10-18 20:55:13 +0900

    42 added typescript linting and husky (#63)
    * configure eslint for typescript
    * linter fixes
    * newer yarn
    * 16.18 node yarn run
    * node 16.18

--- stores/exampleStore.ts
+++ stores/exampleStore.ts
@@ -2,6 +2,7 @@
 import { defineStore } from 'pinia'
 import { useAuthPreferencesStore } from './auth-preferences'
 import { useAuthEmailStore } from './auth-email'
+// eslint-disable-next-line no-unused-vars
 import vuexStore from '@/store' // for gradual conversion, see fullUserDetails
 
 interface State {
@@ -11,54 +12,60 @@ interface State {
 }
 
 export const useAuthUserStore = defineStore('auth/user', {
-  // convert to a function
-  state: (): State => ({
-    firstName: '',
-    lastName: '',
-    userId: null
-  }),
-  getters: {
+    // convert to a function
+    state: (): State => ({
+        firstName: '',
+        lastName: '',
+        userId: null
+    }),
+    getters: {
     // firstName getter removed, no longer needed
-    fullName: (state) => `${state.firstName} ${state.lastName}`,
-    loggedIn: (state) => state.userId !== null,
-    // must define return type because of using `this`
-    fullUserDetails (state): FullUserDetails {
-      // import from other stores
-      const authPreferencesStore = useAuthPreferencesStore()
-      const authEmailStore = useAuthEmailStore()
-      return {
-        ...state,
-        // other getters now on `this`
-        fullName: this.fullName,
-        ...authPreferencesStore.$state,
-        ...authEmailStore.details
-      }
+        fullName: state => `${state.firstName} ${state.lastName}`,
+        loggedIn: state => state.userId !== null,
+        // must define return type because of using `this`
+        // eslint-disable-next-line no-undef
+        fullUserDetails(state): FullUserDetails {
+            // import from other stores
+            const authPreferencesStore = useAuthPreferencesStore()
+            const authEmailStore = useAuthEmailStore()
 
-      // alternative if other modules are still in Vuex
-      // return {
-      //   ...state,
-      //   fullName: this.fullName,
-      //   ...vuexStore.state.auth.preferences,
-      //   ...vuexStore.getters['auth/email'].details
-      // }
-    }
-  },
-  actions: {
+
+            return {
+                ...state,
+                // other getters now on `this`
+                fullName: this.fullName,
+                ...authPreferencesStore.$state,
+                ...authEmailStore.details
+            }
+
+            // alternative if other modules are still in Vuex
+            // return {
+            //   ...state,
+            //   fullName: this.fullName,
+            //   ...vuexStore.state.auth.preferences,
+            //   ...vuexStore.getters['auth/email'].details
+            // }
+        }
+    },
+    actions: {
     // no context as first argument, use `this` instead
-    async loadUser (id: number) {
-      if (this.userId !== null) throw new Error('Already logged in')
-      const res = await api.user.load(id)
-      this.updateUser(res)
-    },
-    // mutations can now become actions, instead of `state` as first argument use `this`
-    updateUser (payload) {
-      this.firstName = payload.firstName
-      this.lastName = payload.lastName
-      this.userId = payload.userId
-    },
-    // easily reset state using `$reset`
-    clearUser () {
-      this.$reset()
+        async loadUser(id: number) {
+            if (this.userId !== null) { throw new Error('Already logged in') }
+            // eslint-disable-next-line no-undef
+            const res = await api.user.load(id)
+
+
+            this.updateUser(res)
+        },
+        // mutations can now become actions, instead of `state` as first argument use `this`
+        updateUser(payload) {
+            this.firstName = payload.firstName
+            this.lastName = payload.lastName
+            this.userId = payload.userId
+        },
+        // easily reset state using `$reset`
+        clearUser() {
+            this.$reset()
+        }
     }
-  }
 })
