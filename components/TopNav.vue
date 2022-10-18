5890a72704fcac7f87d3c4197c2b6b621041f8af
Sulaymane Sillah <sssillah1@gmail.com>
2022-10-18 22:10:04 +0900

    linked hamburger menu from assets

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -10,7 +10,7 @@
                     alt="Buy Me a Coffee at ko-fi.com" />
             </a>
             <div class="cursor-pointer md:hidden">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                <img src="../assets/images/hamburgerMenu.svg" alt="hamburger menu">
             </div>
         </div>
     </div>
