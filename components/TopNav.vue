803b796ab64a94c9f4cefdc51bc97ec1abbd70f6
Sulaymane Sillah <sssillah1@gmail.com>
2022-10-18 22:10:04 +0900

    linked hamburger menu from assets

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -19,7 +19,7 @@
         <div id="searchbar" class="align-middle">
             <SearchBar />
             <div class="cursor-pointer md:hidden">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                <img src="../assets/images/hamburgerMenu.svg" alt="hamburger menu">
             </div>
         </div>
         <nav class="flex gap-4 mx-6 absolute right-20">
