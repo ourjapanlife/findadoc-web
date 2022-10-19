23bbe0ea1288c3d8c15ecea1014c30723caddb58
nasiraistic <nasiranebi914@gmail.com>
2022-10-19 13:08:56 +0900

    remove BuyMeACoffee btn

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -10,10 +10,6 @@
             <NuxtLink to="/aboutPage">
                 About
             </NuxtLink>
-            <a href="https://ko-fi.com/Z8Z14YNUR" target="_blank">
-                <img height="36" style="border: 0px; height: 36px" src="https://cdn.ko-fi.com/cdn/kofi1.png?v=3"
-                    alt="Buy Me a Coffee at ko-fi.com" />
-            </a>
             <div class="cursor-pointer md:hidden">
                 <img src="../assets/images/hamburgerMenu.svg" alt="hamburger menu" />
             </div>
