23bbb0d03d1a2676a42938f260030404cfb268e3
nasiraistic <nasiranebi914@gmail.com>
2022-10-19 21:25:29 +0900

    add language dropdown

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -14,6 +14,13 @@
                 <img src="../assets/images/hamburgerMenu.svg" alt="hamburger menu" />
             </div>
         </div>
+        <div>
+            <select v-model="selected" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >
+                <option disabled value="">Languages</option>
+                <option>English</option>
+                <option>Japanese</option>
+            </select>
+        </div>
     </div>
 </template>
 
