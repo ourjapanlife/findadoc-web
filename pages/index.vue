6a227f964a2a0f60bf7ba70133330ec2a1e08006
Kumaguro3 <lily.sergent@gmail.com>
2022-10-27 21:00:05 +0900

    chore: fix index

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,35 @@
 <template>
-      <div class="h-full">
-            <TopNav />
-            <div class="flex flex-col md:flex-row h-full">
-                  <Welcome />
-                  <FeaturedNews />
-            </div>
-            <div class="w-full">
-                  <Media />
-            </div>
-      </div>
+    <div class="h-full">
+        <TopNav />
+        <div class="flex flex-col md:flex-row h-full">
+            <Welcome />
+            <FeaturedNews />
+        </div>
+        <div class="w-full">
+            <Media />
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
     props: {
-        name: String,
-        msg: { type: String, required: false }
+        name: {
+            type: String,
+            default: '',
+        },
+        msg: {
+            type: String,
+            required: false,
+            default: '',
+        },
     },
     data() {
         return {
-            count: 1
+            count: 1,
         }
     },
-    mounted() {
-    }
+    mounted() {},
 })
 </script>
