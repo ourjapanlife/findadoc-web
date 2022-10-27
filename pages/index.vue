3520791f8af4bd1fef05d31818e8b75a60b428e3
Kumaguro3 <lily.sergent@gmail.com>
2022-10-27 20:54:13 +0900

    chore: checkout pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,29 @@
 <template>
-    <div class="h-full">
-        <TopNav />
-        <div class="flex flex-col md:flex-row h-full">
-            <Welcome />
-            <FeaturedNews />
-        </div>
-        <div class="w-full">
-            <Media />
-        </div>
-    </div>
+      <div class="h-full">
+            <TopNav />
+            <div class="flex flex-col md:flex-row h-full">
+                  <Welcome />
+                  <FeaturedNews />
+            </div>
+            <div class="w-full">
+                  <Media />
+            </div>
+      </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 export default defineComponent({
     props: {
-        name: {
-            type: String,
-            default: "",
-        },
-        msg: {
-            type: String,
-            required: false,
-            default: "",
-        },
+        name: String,
+        msg: { type: String, required: false }
     },
     data() {
         return {
-            count: 1,
-        };
+            count: 1
+        }
     },
-    mounted() {},
-});
+    mounted() {
+    }
+})
 </script>
