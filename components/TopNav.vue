c3162a4289882b021cdea3fec124550a6c680489
Sulaymane Sillah <99618731+Tch4lla@users.noreply.github.com>
2022-10-30 21:36:05 +0900

    Tchalla/hamburger component (#99)
    * added hamburger component
    
    * added css to spans, created hamburger component
    
    * ran yarn lint to correct lint issues

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -11,30 +11,8 @@
                 About Us
             </NuxtLink>
 
-            <div class="hidden cursor-pointer md:hidden">
-                <img @click="toggleMenu"
-                    src="../assets/images/hamburgerMenu.svg"
-                    alt="hamburger menu" />
-                <div v-if="showMenu">
-                    <div>
-                        <span class="block">Home</span>
-                        <span class="block">About</span>
-                    </div>
-                </div>
-            </div>
+            <HamburgerComponent />
         </div>
         <LocaleSelector />
     </div>
 </template>
-
-<script setup>
-import { ref } from 'vue'
-
-//state
-const showMenu = ref(false)
-
-//functions
-function toggleMenu() {
-    showMenu.value = !showMenu.value
-}
-</script>
