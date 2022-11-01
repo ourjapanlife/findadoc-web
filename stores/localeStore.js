5992f81f6af34def8f9db5d20220ea64969b9dee
nasiraistic <nasiranebi914@gmail.com>
2022-11-01 11:28:47 +0900

    localePinia

---
+++ stores/localeStore.js
@@ -0,0 +1,21 @@
+import { defineStore } from "pinia";
+
+export const useLocaleStore = defineStore("locale", {
+    state: () => ({
+        locale: { lable: "English", code: "en" },
+    }),
+    getters: {
+        currentLocale: (state) => state.locale,
+    },
+    actions: {
+        changeLocale() {
+            if (this.currentLocale.code === "en") {
+                this.locale.lable = "日本語";
+                this.locale.code = "ja";
+            } else {
+                this.locale.lable = "English";
+                this.locale.lable = "en";
+            }
+        },
+    },
+});
