7992b0591540a27beff4fee5cf1f13393e823606
Olivier Lechevalier <olivier.lechevalier@gmail.com>
2022-11-03 11:15:05 +0900

    debug

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,6 +2,10 @@ describe('Visits the home page', () => {
     before(() => {
         cy.catchNuxtProxyError()
         cy.visit('/')
+        cy.on('uncaught:exception', err => {
+            // eslint-disable-next-line no-console
+            console.log(err)
+        })
     })
     it('Displays the Hero Section', () => {
         cy.get('.h-full ').contains('The open-source project that\'s helping Japan get vaccinated')
