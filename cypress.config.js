02b99eee8ab7b560d063befc7382f422a6e1100d
Olivier Lechevalier <olivier.lechevalier@gmail.com>
2022-11-03 10:57:17 +0900

    Add cypress test to make build green

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     e2e: {
+        baseUrl: 'http://localhost:3000'
         // eslint-disable-next-line no-unused-vars
         // setupNodeEvents(on, config) {
         //     // implement node event listeners here
---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,9 @@
+describe('Visits the home page', () => {
+    before(() => {
+        cy.catchNuxtProxyError()
+        cy.visit('/')
+    })
+    it('Displays the Hero Section', () => {
+        cy.get('.h-full ').contains('The open-source project that\'s helping Japan get vaccinated')
+    })
+})
