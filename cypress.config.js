692dc51e118b9992ecfcdf901e6e5a2b16f1b4be
Olivier Lechevalier <olivier.lechevalier@gmail.com>
2022-11-03 12:27:59 +0900

    ci(e2e): remove cypress.config.js

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    e2e: {
-        baseUrl: 'http://localhost:3000'
-        // eslint-disable-next-line no-unused-vars
-        // setupNodeEvents(on, config) {
-        //     // implement node event listeners here
-        // },
-    }
-}
