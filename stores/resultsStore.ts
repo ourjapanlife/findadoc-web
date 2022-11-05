2890a4428e0b0807948bbc4bdf1afa14b5de2dac
Sulaymane Sillah <sssillah1@gmail.com>
2022-11-05 15:52:42 +0900

    building results store

---
+++ stores/resultsStore.ts
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia'
+
+export const useResultsStore = defineStore('resultsStore', {
+    state:() => ({
+        results: [
+            {
+                facilityID : Math.floor(Math.random() * 25000), 
+                name: 'English Doctor',
+                activeResult : false
+            },
+            {
+                facilityID : Math.floor(Math.random() * 25000), 
+                name: 'Spanish Doctor',
+                activeResult : false
+            },
+            {
+                facilityID : Math.floor(Math.random() * 25000), 
+                name: 'Italian Doctor',
+                activeResult : false
+            },
+        ]
+        
+        
+    }),
+    actions : {
+        getResults(){
+            return this.results
+        },
+
+        setActiveResult(facilityID: number){
+            const result = this.results.find(result => result.facilityID === facilityID)
+            result.activeResult = true
+        },
+
+        clearResults(facilityID: number){
+            const resultToClear = this.results.find(result => result.facilityID === facilityID)
+            resultToClear.activeResult = false
+        }
+    }
+})
