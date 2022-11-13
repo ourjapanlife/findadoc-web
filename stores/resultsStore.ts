259469fa4fa4672dedcc4ece2de8fc46a58f3b86
Sulaymane Sillah <sssillah1@gmail.com>
2022-11-13 16:45:12 +0900

    yarn lint fixes

--- stores/resultsStore.ts
+++ stores/resultsStore.ts
@@ -1,45 +1,44 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+const defaultState = {
+    results: ref([]), 
+    error: ref(null)
+}
 
 export const useResultsStore = defineStore('resultsStore', {
-    state:() => ({
-        results: [
-            {
-                facilityID : Math.floor(Math.random() * 25000), 
-                name: 'English Doctor',
-                activeResult : false
-            },
-            {
-                facilityID : Math.floor(Math.random() * 25000), 
-                name: 'Spanish Doctor',
-                activeResult : false
-            },
-            {
-                facilityID : Math.floor(Math.random() * 25000), 
-                name: 'Italian Doctor',
-                activeResult : false
-            },
-        ]
-        
-        
-    }),
-    actions : {
-        getResults(){
-            return this.results
-        },
+    state: () => (defaultState),
+    actions: {
 
-        // setActiveResult(facilityID: number){
-        //     const result = this.results.find(result => result.facilityID === facilityID)
-        //     result.activeResult = true
+        // async fetchResults(searchParams: string) {
+        //     return this.results = await fetch.get(((`${serverBaseUrl }/results`, searchParams)))
         // },
 
-        // clearResults(facilityID: number){
-        //     const resultToClear = this.results.find(result => result.facilityID === facilityID)
-        //     resultToClear.activeResult = false
-        // }
+        async fetchResults(searchParams: string): Promise<any> {
+            try {
+                const data = await fetch(`http://localhost:3000/dummyEndpoint/${searchParams}`)
 
-        toggleActiveResult(facilityID: number){
-            const result = this.results.find(result => result.facilityID === facilityID)
-            return result!.activeResult = !result!.activeResult
+                if (!data.ok) {
+                    throw Error ('This data is not available')
+                }
+                defaultState.results = await data.json()
+            } catch (error: any) {
+                defaultState.error.value = error.message                
+            }
+
+            return defaultState.results
+        },
+
+        // dislikeResult(resultId: any) {
+        //     this.results = this.results.filter(result => result.id !== resultId)
+        // },
+
+        resetState() {
+            defaultState.results = ref([])
+        },
+
+        toggleActiveResults(state: any) {
+            defaultState.results.value = defaultState.results.value.filter((results: any) => state.results.value === results)
         }
     }
 })
