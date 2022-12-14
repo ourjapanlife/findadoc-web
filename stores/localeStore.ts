9c49b87223163febeb8d3aa2f9e1144c0f6546d4
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2022-12-14 22:12:54 +0900

    localePinia (#104)
    * localePinia
    
    * localePinia
    
    * localeStore to ts
    
    * Rename localeStore.js to localeStore.ts
    
    * Update localeStore.ts
    
    Delete blank lines at the end

---
+++ stores/localeStore.ts
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia'
+
+const localeEn:{label: string, code: string} = {
+    label: 'English',
+    code: 'en'
+}
+const localeJa:{label: string, code: string} = {
+    label: '日本語',
+    code: 'ja'
+}
+
+export const useLocaleStore = defineStore('locale', {
+    state: () => ({
+        locale: localeEn
+    }),
+    getters: {
+        currentLocale: state => state.locale
+    },
+    actions: {
+        changeLocale() {
+            if (this.currentLocale.code === 'en') {
+                this.locale = localeEn
+            } else {
+                this.locale = localeJa
+            }
+        }
+    }
+})
