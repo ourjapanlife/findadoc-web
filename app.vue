43276f00b9ff993cac47744a87d0519371b1aa24
Ann 杏 Kilzer <akilzer@gmail.com>
2022-12-15 21:50:21 +0900

    add app.vue for top level structure, simplify some names

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+    <TopNav />
+    <NuxtPage />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
---
+++ components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <div>Deployed on Netlify</div>
+        <div>
+            Contribute on <a href="https://github.com/ourjapanlife">GitHub</a>
+        </div>
+    </div>
+</template>
