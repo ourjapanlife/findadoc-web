76c9f675554a11badb3b992693119c21da58d4b4
Ann 杏 Kilzer <akilzer@gmail.com>
2022-12-15 22:27:43 +0900

    disable prettier

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="h-full">
         <TopNav />
-        <div class="flex flex-col md:flex-row h-full">Login!</div>
+        <div class="flex flex-col md:flex-row h-full">
+            Login!
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-export default defineComponent({});
+import { defineComponent } from 'vue'
+export default defineComponent({})
 </script>
