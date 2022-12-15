e3c46a6b31571de320855c19e6adc0cd1b6d155f
Ann 杏 Kilzer <akilzer@gmail.com>
2022-12-15 21:18:01 +0900

    clean up about

--- pages/aboutPage.vue
+++ pages/aboutPage.vue
@@ -3,13 +3,6 @@
         <TopNav />
         <div class="flex flex-col md:flex-row h-full">
             About us!
-
-            <a href="https://ko-fi.com/Z8Z14YNUR" target="_blank">
-                <img height="36"
-                    style="border: 0px; height: 36px"
-                    src="https://cdn.ko-fi.com/cdn/kofi1.png?v=3"
-                    alt="Buy Me a Coffee at ko-fi.com" />
-            </a>
         </div>
     </div>
 </template>
