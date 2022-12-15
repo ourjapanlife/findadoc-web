4515ef3556d64a93c3da831691761036fcf715f0
Ann 杏 Kilzer <akilzer@gmail.com>
2022-12-15 22:21:10 +0900

    add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     content: [
         './components/**/*.{js,vue,ts}',
         './layouts/**/*.vue',
         './pages/**/*.vue',
         './plugins/**/*.{js,ts}',
-        './nuxt.config.{js,ts}'
+        './nuxt.config.{js,ts}',
+        './app.vue'
     ],
     theme: {
         screens: {
             md: { min: '800px' }
         },
-        extend: {}
+        extend: {},
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            emerald: colors.emerald,
+            indigo: colors.indigo,
+            yellow: colors.yellow,
+            teal: colors.teal,
+            slate: colors.slate,
+            zinc: colors.zinc
+        }
     },
     plugins: []
 }
