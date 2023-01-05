71e434d5c797aa865c141e006341c3ade88b508b
Philip Ermish <philipermish@gmail.com>
2023-01-05 18:44:53 +0900

    fixed cypress test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,6 +3,6 @@ describe('Visits the home page', () => {
         cy.visit('/')
     })
     it('Displays the Hero Section', () => {
-        cy.get('h1').contains('Connecting people in Japan to the healthcare services they need, in the languages they need.')
+        cy.get('#search-button').contains('Search')
     })
 })
