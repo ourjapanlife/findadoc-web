0005fb873f5249dc0f8c67a92aab0f47511c6d73
Ann 杏 Kilzer <akilzer@gmail.com>
2023-01-08 20:49:15 +0900

    shorten line

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,6 +3,8 @@ describe('Visits the home page', () => {
         cy.visit('/')
     })
     it('Displays the Hero Section', () => {
-        cy.get('h1').contains('Connecting people in Japan to the healthcare services they need, in the languages they need.')
+        cy.get('h1').contains(
+            'Connecting people in Japan to the healthcare services they need, in the languages they need.'
+        )
     })
 })
