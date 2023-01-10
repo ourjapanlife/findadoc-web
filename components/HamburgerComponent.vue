f7becc774d84d3df1732c7674d4791383d1481f3
Sulaymane Sillah <sssillah1@gmail.com>
2023-01-11 00:03:10 +0900

    Fixed logic for results store
    Co-authored-by: Philip Ermish <philipermish@gmail.com>

--- components/HamburgerComponent.vue
+++ components/HamburgerComponent.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="cursor-pointer md:hidden">
-        <img @click="toggleMenu"
-            src="../assets/images/hamburger-menu.svg"
-            alt="hamburger menu" />
+        <img @click="toggleMenu" src="../assets/images/hamburger-menu.svg" alt="hamburger menu" />
         <div v-if="showMenu">
             <div>
                 <span class="block hover:-translate-y-1 hover:scale-110 duration-300">Home</span>
@@ -10,6 +8,7 @@
             </div>
         </div>
     </div>
+
 </template>
 
 <script setup lang="ts">
