1a763603abd32de87331eca672f34632e978e106
Ann Kilzer キルザー杏 <akilzer@gmail.com>
2023-01-31 23:00:13 +0900

    Sync Eslint between frontend and backend (#152)
    * 2 => 'error'
    
    * 0 => 'off'
    
    * 1 => 'warn'
    
    * make eslint closer to backend
    
    * fix line length
    
    * shorten line
    
    * Apply suggestions from code review
    
    * fix stuff
    
    * lint fixes
    
    ---------
    
    Co-authored-by: Philip Ermish <philipermish@gmail.com>

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="p-4 pl-8 border border-secondary-bg/20 flex justify-between items-center bg-gradient-to-t from-secondary-bg/30 via-primary-bg to-primary-bg">
+    <div class="p-4 pl-8 border border-secondary-bg/20 flex justify-between items-center
+        bg-gradient-to-t from-secondary-bg/30 via-primary-bg to-primary-bg">
         <HamburgerComponent />
         <div class="font-semibold text-xl group transition-colors">
             <NuxtLink class="flex" to="/">
-                <svg role="img" title="site icon" class="mr-1 w-10 h-10 align-middle fill-primary group-hover:fill-primary-hover">
+                <svg role="img" title="site icon"
+                    class="mr-1 w-10 h-10 align-middle fill-primary group-hover:fill-primary-hover">
                     <use xlink:href="../assets/images/site-logo.svg#site-logo-svg" />
                 </svg>
                 <div class="title-text flex flex-col">
