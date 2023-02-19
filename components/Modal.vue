71d85270996080311e7df86c26c32f81250ca838
Philip Ermish <philipermish@gmail.com>
2023-02-19 22:41:09 +0900

    css improvements. added hover status

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="absolute z-10 bg-secondary-bg rounded-lg overflow-hidden
+    <div class="absolute z-10 bg-secondary-bg rounded-xl overflow-hidden
         hover:shadow-inner hover:shadow-secondary-bg/90">
         <div v-if="isOpen" class="modal">
             <button @click="closeModal"
-                class="close-button absolute right-5 top-5 h-6 bg-primary-inverted rounded-lg px-1.5 py-.5">
-                <span class="close-icon text-primary-text-inverted">
-                    <svg class="" width="16" heigh="16" viewBox="4 0 16 25">
-                        <path stroke="black" stroke-width="4" fill="none" d="M6.25,6.25,17.75,17.75" />
-                        <path stroke="black" stroke-width="4" fill="none" d="M6.25,17.75,17.75,6.25" />
+                class="close-button absolute right-6 top-5 bg-primary-inverted rounded-lg px-2 py-.5
+                group hover:bg-primary-hover transition-all duration-200">
+                <span class="close-icon">
+                    <svg class="stroke-primary group-hover:stroke-primary-inverted" width="20" heigh="20" viewBox="4 0 15 25">
+                        <path stroke-width="3" fill="none" d="M6.25,6.25,17.75,17.75" />
+                        <path stroke-width="3" fill="none" d="M6.25,17.75,17.75,6.25" />
                     </svg>
                 </span>
             </button>
@@ -26,3 +27,4 @@ const closeModal = () => {
 }
 
 </script>
+
