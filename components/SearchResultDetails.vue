0b40ac5226bb75494d79c7e0383b87c5efb7d297
Philip Ermish <philipermish@gmail.com>
2023-02-20 16:51:55 +0900

    linter fix

--- components/SearchResultDetails.vue
+++ components/SearchResultDetails.vue
@@ -84,7 +84,7 @@ const phone = ref(dummyData.phone)
 
 <style>
 .hamburger-list-icon {
-    --background-color: transparent;
-    --second-background-color: --color-bg-secondary;
+  --background-color: transparent;
+  --second-background-color: --color-bg-secondary;
 }
 </style>
