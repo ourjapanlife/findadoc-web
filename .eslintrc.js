6012c0132407c326c9fb2105ef37b9f318179cc8
Philip Ermish <philipermish@gmail.com>
2023-02-20 23:16:21 +0900

    it works!! now generating types from gql api (#218)

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     plugins: ['@typescript-eslint', 'eslint-plugin-json', 'cypress'],
     extends: ['eslint:recommended', 'plugin:vue/vue3-strongly-recommended'],
     root: true,
+    ignorePatterns: ['typedefs/gqlTypes.ts'],
     // 'off' or 0 - turn the rule off
     // 'warn' or 1 - turn the rule on as a warning (doesn’t affect exit code)
     // 'error' or 2 - turn the rule on as an error (exit code will be 1)
