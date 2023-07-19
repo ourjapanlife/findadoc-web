066b94f474bebbbc7239fe5ee56eeccfbdc7f3d0
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2023-07-19 22:17:21 +0900

    Use Pinia State for Modals (#267)

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="absolute z-10 bg-primary-bg rounded-xl overflow-hidden
         hover:shadow-inner hover:shadow-secondary-bg/90">
-        <div v-if="isOpen" class="modal">
-            <button @click="closeModal"
+        <div class="modal">
+            <button @click="store.hideModal()"
                 class="close-button absolute right-6 top-5 bg-primary-inverted rounded-lg px-2 py-.5
                 group hover:bg-primary-hover transition-all duration-200">
                 <span class="close-icon">
@@ -18,13 +18,9 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { useModalStore } from '~/stores/modalStore'
 
-const isOpen = ref(true)
-
-const closeModal = () => {
-    isOpen.value = false
-}
+const store = useModalStore()
 
 </script>
 
