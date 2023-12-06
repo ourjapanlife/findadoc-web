5fab9ddd1391c9d9798a434fe334cefb3850d127
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2023-12-06 09:20:54 +0900

    chore: update localization and credentials (#301)
    * chore: update localization for footer
    
    * wip

--- components/MapContainer.vue
+++ components/MapContainer.vue
@@ -1,47 +1,53 @@
 <template>
-    <GoogleMap data-testid="map-of-japan" ref="mapRef" :api-key="runtimeConfig.public.googleMapsApiKey"
-        map-id="153d718018a2577e"
-        style="height: 100%; width: 100%;"
-        :center="center"
-        :zoom="9">
-        <GMarker @click="store.setActiveSearchResult(location.id)" :options="{position: location.position, icon: markerIcon}"
-            :key="index" v-for="(location, index) in store.searchResultsList" />
-    </GoogleMap>
+  <GoogleMap
+    data-testid="map-of-japan"
+    ref="mapRef"
+    :api-key="runtimeConfig.public.GOOGLE_MAPS_API_KEY"
+    map-id="153d718018a2577e"
+    style="height: 100%; width: 100%;"
+    :center="center"
+    :zoom="9"
+  >
+    <GMarker
+      @click="store.setActiveSearchResult(location.id)"
+      :options="{position: location.position, icon: markerIcon}"
+      :key="index"
+      v-for="(location, index) in store.searchResultsList"
+    />
+  </GoogleMap>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from 'vue'
-import { GoogleMap, Marker as GMarker } from 'vue3-google-map'
-import { useRuntimeConfig } from '#imports'
-import customIcon from '../assets/images/blue-map-pin.svg'
-import { useSearchResultsStore } from '../stores/searchResultsStore'
+import { defineComponent, ref, computed } from "vue";
+import { GoogleMap, Marker as GMarker } from "vue3-google-map";
+import { useRuntimeConfig } from "#imports";
+import customIcon from "../assets/images/blue-map-pin.svg";
+import { useSearchResultsStore } from "../stores/searchResultsStore";
 
 export default defineComponent({
-    components: { GoogleMap, GMarker },
-    setup() {
-        const defaultLocation = { lat: 35.6804, lng: 139.7690 }
-        const center = computed(() => useSearchResultsStore().$state.activeResult?.position || defaultLocation)
+  components: { GoogleMap, GMarker },
+  setup() {
+    const defaultLocation = { lat: 35.6804, lng: 139.769 };
+    const center = computed(
+      () =>
+        useSearchResultsStore().$state.activeResult?.position || defaultLocation
+    );
 
-        const markerIcon = {
-            url: customIcon,
-            scaledSize: {
-                width: 45,
-                height: 73
-            }
-        }
+    const markerIcon = {
+      url: customIcon,
+      scaledSize: {
+        width: 45,
+        height: 73
+      }
+    };
 
-        const store = useSearchResultsStore()
+    const store = useSearchResultsStore();
 
-        const mapRef = ref(null)
+    const mapRef = ref(null);
 
-        const runtimeConfig = useRuntimeConfig()
+    const runtimeConfig = useRuntimeConfig();
 
-        return { center,
-            mapRef,
-            markerIcon,
-            runtimeConfig,
-            store }
-    }
-})
-
+    return { center, mapRef, markerIcon, runtimeConfig, store };
+  }
+});
 </script>
