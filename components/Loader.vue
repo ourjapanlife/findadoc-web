ac15309e23479b62c84b1ca622a39864c2fbf109
Philip Ermish <philipermish@gmail.com>
2023-12-06 20:55:22 +0900

    apollo query works and loader

--- components/Loader.vue
+++ components/Loader.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex align-middle justify-center" v-show="!store.isLoading">
-        <span class="text-primary mx-3">Loading</span>
+    <div class="h-full w-full flex align-middle justify-around bg-gray-200/50 z-10 absolute" v-show="store.isLoading">
+        <div class="flex w-full text-primary mx-3 text-4xl self-center">Loading...</div>
         <svg role="img" alt="loading animation" title="loading animation"
-            class="loading-icon fill-primary h-5 w-5 self-center group-hover:fill-primary-text-inverted">
+            class="loading-icon fill-primary h-25 w-25 self-center group-hover:fill-primary-text-inverted">
             <use xlink:href="../assets/images/loading.svg#loading-svg" />
         </svg>
     </div>
