56139f11e9edeebcba3aa19821b237a79a6b5048
Philip Ermish <philipermish@gmail.com>
2023-12-07 06:26:47 +0900

    fixing prod build

--- app.vue
+++ app.vue
@@ -6,8 +6,8 @@
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { initializeApollo } from './utils/apollo.js'
 
 export default defineComponent({})
 </script>
