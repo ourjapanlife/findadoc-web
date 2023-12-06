740ad2852b9e922a29a825e797a6977efdd5c8c0
theyokohamalife <lts001@gmail.com>
2023-12-06 20:54:40 +0900

    localization working

--- i18n.options.ts
+++ i18n.options.ts
@@ -3,7 +3,7 @@ import i18n from './i18n/index.js'
 //@ts-ignore-next-line
 export default defineI18nConfig(() => ({
     legacy: false,
-    locale: 'en',
-    fallbackLocale: 'en-US',
+    locale: 'en_US',
+    fallbackLocale: 'en_US',
     messages: i18n
 }))
