d4308532f0b400a9397dfdcac68135911c2bd20f
theyokohamalife <lts001@gmail.com>
2023-12-07 15:06:59 +0900

    add terms and conditions

--- pages/terms.vue
+++ pages/terms.vue
@@ -1,41 +1,200 @@
 <template>
-    <div class="h-full">
-        <div class="flex flex-col md:flex-col h-full">
-            <h1>Terms and conditions</h1>
-            <p>Coming soon</p>
-            <h3 class="py-5">
-                Licensing
-            </h3>
-            <p>
-                BSD 3-Clause License
-
-                Copyright (c) 2022, Our Japan Life
-                All rights reserved.
-
-                Redistribution and use in source and binary forms, with or without
-                modification, are permitted provided that the following conditions are met:
-
-                1. Redistributions of source code must retain the above copyright notice, this
-                list of conditions and the following disclaimer.
-
-                2. Redistributions in binary form must reproduce the above copyright notice,
-                this list of conditions and the following disclaimer in the documentation
-                and/or other materials provided with the distribution.
-
-                3. Neither the name of the copyright holder nor the names of its
-                contributors may be used to endorse or promote products derived from
-                this software without specific prior written permission.
-
-                THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-                AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-                IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-                DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-                FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-                DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-                SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-                CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-                OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-                OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    <div class="flex flex-col place-items-center mx-10">
+        <div>
+            <h1
+                class="mb-20 text-black text-5xl font-bold font-['Noto Sans JP']"
+            >
+                Terms and Conditions
+            </h1>
+            <p
+                class="mb-10 text-rose-400 text-3xl font-bold font-['Noto Sans JP']"
+            >
+                1. Acceptance of Terms
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                By accessing or using Find a Doc, Japan, you agree to comply
+                with and be bound by these terms and conditions. If you do not
+                agree with any part of these terms, you should not use the
+                website.
+            </p>
+            <p
+                class="mb-10 text-rose-400 text-3xl font-bold font-['Noto Sans JP']"
+            >
+                2. Use of the Website
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                By accessing or using Find a Doc, Japan, you agree to comply
+                with and be bound by these terms and conditions. If you do not
+                agree with any part of these terms, you should not use the
+                website.
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                a. Eligibility
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                You must be at least 18 years old to use Find a Doc, Japan.
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                b. User Accounts
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                In order to access certain features of the website, you may be
+                required to create a user account. You are responsible for
+                maintaining the confidentiality of your account information and
+                for all activities that occur under your account.
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                c. Accuracy of Information
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                Users are responsible for the accuracy of the information
+                provided, including adding new hospitals and doctors to the
+                website. Misleading or false information may result in the
+                suspension of your account.
+            </p>
+            <p
+                class="mb-10 text-rose-400 text-3xl font-bold font-['Noto Sans JP']"
+            >
+                3. User-Provided Content
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                a. Hospital and Doctor Information
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                Users can contribute by adding new hospitals and doctors to the
+                website. By submitting such information, you warrant that it is
+                accurate, up-to-date, and complies with all applicable laws.
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                b. Rights to User-Provided Content
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                By submitting content to Finda a Doc, Japan, you grant the
+                website a non-exclusive, royalty-free, worldwide license to use,
+                reproduce, modify, distribute, and display the content.
+            </p>
+            <p
+                class="mb-10 text-rose-400 text-3xl font-bold font-['Noto Sans JP']"
+            >
+                4. Privacy
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                a. Privacy Policy
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                Your use of Find a Doc, Japan is also governed by our Privacy
+                Policy, which can be found
+                <NuxtLink
+                    to="/privacypolicy"
+                    class=""
+                    data-testid="privacy-link"
+                    >here</NuxtLink
+                >. Please review the Privacy Policy to understand our practices.
+            </p>
+            <p
+                class="mb-10 text-rose-400 text-3xl font-bold font-['Noto Sans JP']"
+            >
+                5. Limitation of Liability
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                a. Disclaimer
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                Find a Doc, Japan provides information about hospitals and
+                doctors as a service to users. We do not endorse or guarantee
+                the accuracy of the information provided on the website. Users
+                are encouraged to verify information independently.
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                b. No Medical Advice
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                Find a Doc, Japan does not provide medical advice. Users are
+                advised to consult with qualified healthcare professionals for
+                medical advice and assistance.
+            </p>
+            <p
+                class="mb-10 text-rose-400 text-3xl font-bold font-['Noto Sans JP']"
+            >
+                6. Termination
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                Find a Doc, Japan reserves the right to terminate or suspend
+                your account and access to the website at any time without
+                notice, for any reason, including a breach of these terms.
+            </p>
+            <p
+                class="mb-10 text-rose-400 text-3xl font-bold font-['Noto Sans JP']"
+            >
+                7. Changes to Terms and Conditions
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                Find a Doc, Japan reserves the right to update or modify these
+                terms and conditions at any time without prior notice. Users are
+                encouraged to review the terms regularly.
+            </p>
+            <p
+                class="mb-10 text-rose-400 text-3xl font-bold font-['Noto Sans JP']"
+            >
+                8. Governing Law
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                These terms and conditions are governed by and construed in
+                accordance with the laws of Japan.
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                If you have any questions or concerns about these terms and
+                conditions, please contact us
+                <a href="https://forms.gle/4E763qfaq46kEsn99">here</a>.
+            </p>
+            <p
+                class="mb-4 text-gray-500 text-lg font-normal font-['Noto Sans JP']"
+            >
+                Thank you for using Find a Doc, Japan!
             </p>
         </div>
     </div>
