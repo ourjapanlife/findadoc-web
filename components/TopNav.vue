0bb89693b9df969317fafe3478a0929cf673ee58
Philip Ermish <philipermish@gmail.com>
2023-12-07 15:21:10 +0900

    fixing responsiveness of navbar. site logo

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="p-4 border border-secondary-bg/20 flex justify-around items-center bg-gradient-to-t from-secondary-bg/30 via-primary-bg to-primary-bg"
+    class="p-4 pl-8 border border-secondary-bg/20 flex justify-between items-center bg-gradient-to-t from-secondary-bg/30 via-primary-bg to-primary-bg"
   >
     <HamburgerComponent />
     <div class="font-semibold text-xl group transition-colors items-start">
@@ -8,11 +8,11 @@
         <svg
           role="img"
           title="site icon"
-          class="mr-1 w-10 h-10 align-middle fill-primary group-hover:fill-primary-hover"
+          class="mr-1 w-10 h-10 flex-shrink-0 align-middle fill-primary group-hover:fill-primary-hover"
         >
           <use xlink:href="../assets/images/site-logo.svg#site-logo-svg" />
         </svg>
-        <div class="title-text flex flex-col" data-testid="logo">
+        <div class="title-text flex flex-col flex-shrink-0" data-testid="logo">
           <div class="text-lg text-primary group-hover:text-primary-hover">Find a Doc</div>
           <div class="text-sm text-primary leading-none group-hover:text-primary-hover">Japan</div>
         </div>
