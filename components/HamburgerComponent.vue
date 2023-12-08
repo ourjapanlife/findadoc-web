6d0136b808a1a52891798cd06bf50a44157478a2
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2023-12-09 01:02:03 +0900

    fix: make top nav responsive (#318)

--- components/HamburgerComponent.vue
+++ components/HamburgerComponent.vue
@@ -1,25 +1,41 @@
 <template>
-    <div class="cursor-pointer md:hidden">
-        <img @click="toggleMenu"
-            src="../assets/images/hamburger-menu.svg"
-            alt="hamburger menu" />
-        <div v-if="showMenu">
+    <div class="cursor-pointer md:hidden w-full">
+        <div class="flex justify-between w-full">
             <div>
-                <span class="block hover:-translate-y-1 hover:scale-110 duration-300">Home</span>
-                <span class="block hover:-translate-y-1 hover:scale-110 duration-300">About</span>
+                <img
+                    @click="menuStore.toggleMenu()"
+                    src="../assets/images/hamburger-menu.svg"
+                    alt="hamburger menu"
+                />
+                <div
+                    v-show="menuStore.menuOpen"
+                    class="z-20 absolute top-[50px] w-[200px] p-8 rounded bg-white border-2"
+                >
+                    <div
+                        v-for="(item, index) in menuStore.menuItems"
+                        :key="index"
+                    >
+                        <NuxtLink :to="item.route">
+                            <span
+                                @click="menuStore.toggleMenu()"
+                                class="block hover:-translate-y-1 hover:scale-110 duration-300"
+                                >{{ $t(item.displayText) }}</span
+                            >
+                        </NuxtLink>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <span class="text-black text-md font-bold font-['Noto Sans JP']"
+                    >Find a Doc, Japan</span
+                >
             </div>
         </div>
     </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="ts" setup>
+import { useMenuStore } from "~/stores/menuStore";
 
-//state
-const showMenu = ref(false)
-
-//functions
-function toggleMenu() {
-    showMenu.value = !showMenu.value
-}
+const menuStore = useMenuStore();
 </script>
---
+++ stores/menuStore.ts
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia"
+import { ref, watch } from 'vue'
+
+export const useMenuStore = defineStore('menuStore', () => {
+    const menuOpen = ref(false)
+    const menuItems = {
+        "home": { "displayText": "topNav.home", "route": "/" },
+        "about": { "displayText": "topNav.about", "route": "/about" },
+        "submit": { "displayText": "topNav.submit", "route": "/submit" },
+    }
+
+    function toggleMenu() {
+        console.log("menu= ", menuOpen.value)
+        menuOpen.value = !menuOpen.value
+    }
+
+    return {
+        menuItems,
+        menuOpen,
+        toggleMenu
+    }
+})
