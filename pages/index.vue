c22e89354959ed7e4fcb6a871b0b3a78e2de3bac
Sulaymane Sillah <sssillah1@gmail.com>
2023-12-13 07:10:04 +0900

    Update screen to be responsive on mobile
    breakpoint added at screen with 640px. Will extend width of left navbar
    to 100% and place map at bottom

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,25 @@
 import MainContentContainer from '~/components/MainContentContainer.vue';
 
 <template>
-    <div class="flex overflow-hidden">
-        <LeftNavbar class="flex-0 bg-primary-bg w-[358px]" />
-        <div class="flex-1">
+    <div class="content-container flex overflow-hidden">
+        <LeftNavbar class="left-container sm:order-2 flex-0 bg-primary-bg w-[358px]" />
+        <div class="main-content-container sm:-order-1 flex-1">
             <div class="flex flex-col md:flex-row h-full">
                 <MainContentContainer />
             </div>
         </div>
     </div>
 </template>
+
+<style>
+@media screen and (max-width: 640px) {
+    .content-container {
+        flex-direction: column;
+    }
+
+    .left-container {
+        width: 100%;
+
+    }
+}
+</style>
