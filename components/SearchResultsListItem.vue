53a7005626f8d9a892db829cb6a507ddee79c393
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2023-12-13 22:14:24 +0900

    Make language tags easier to understand (#337)
    * chore: make language tags easier to understand
    
    * chore: make language tags easier to understand
    
    * linting fixes
    
    * removing semicolons
    
    ---------
    
    Co-authored-by: Philip Ermish <philipermish@gmail.com>

--- components/SearchResultsListItem.vue
+++ components/SearchResultsListItem.vue
@@ -7,14 +7,22 @@
             <span class="w-4 font-bold pl-2 self-center"> {{ name }} </span>
         </div>
         <div class="result-details flex flex-col my-2 ml-4">
-            <span class="px-2 text-primary font-medium"> {{ facilityName }}</span>
-            <div class="flex" v-for="(specialty, index) in formattedSpecialties">
+            <span class="px-2 text-primary font-medium">
+                {{ facilityName }}</span>
+            <div class="flex" v-for="(specialty, index) in formattedSpecialties" :key="index">
                 <span class="ml-2 pr-2 italic"> {{ specialty }}</span>
             </div>
-            <div class="result-tags flex flex-wrap w-64 mb-2 mt-3 ml-1">
-                <div :key="index" v-for="(spokenLanguage, index) in formattedLanguages"
-                    class="px-2 py-[1px] mr-2 border border-primary/40 rounded-full shadow text-sm hover:bg-primary/20 transition-all">
-                    {{ spokenLanguage }}
+
+            <div class="flex ml-2 mt-4">
+                <svg role="img" alt="chat bubbles icon" title="chat bubbles icon"
+                    class="chat-bubbles-icon w-8 h-8 stroke-primary inline">
+                    <use xlink:href="../assets/images/chat-bubbles.svg#chat-bubbles-icon-svg" class="w-[20px] h-[20px]" />
+                </svg>
+                <div class="result-tags flex flex-wrap w-64 mb-2 mt-1 ml-1">
+                    <div :key="index" v-for="(spokenLanguage, index) in formattedLanguages"
+                        class="px-2 py-[1px] mr-2 border border-primary/40 rounded-full shadow text-sm hover:bg-primary/20 transition-all">
+                        {{ spokenLanguage }}
+                    </div>
                 </div>
             </div>
         </div>
@@ -32,41 +40,42 @@ const specialtiesStore = useSpecialtiesStore()
 const props = defineProps({
     name: {
         type: String,
-        required: true
+        required: true,
     },
     specialties: {
         type: Array,
-        required: true
+        required: true,
     },
     facilityName: {
         type: String,
-        required: true
+        required: true,
     },
     spokenLanguages: {
         type: Array,
-        required: true
-    }
+        required: true,
+    },
 })
 
-const formattedSpecialties = computed(
-    () => {
-        const specialtiesDisplayText = props.specialties?.map(s => {
-            const specialtyDisplayText = specialtiesStore.specialtyDisplayOptions.find(l => l.code === s)?.displayText
-            return specialtyDisplayText
-        })
+const formattedSpecialties = computed(() => {
+    const specialtiesDisplayText = props.specialties?.map((s) => {
+        const specialtyDisplayText =
+            specialtiesStore.specialtyDisplayOptions.find(
+                (l) => l.code === s
+            )?.displayText
+        return specialtyDisplayText
+    })
 
-        return specialtiesDisplayText
-    }
-)
+    return specialtiesDisplayText
+})
 
-const formattedLanguages = computed(
-    () => {
-        const languagesDisplayText = props.spokenLanguages?.map(s => {
-            const languageDisplayText = localStore.localeDisplayOptions.find(l => l.code === s)?.simpleText
-            return languageDisplayText
-        })
+const formattedLanguages = computed(() => {
+    const languagesDisplayText = props.spokenLanguages?.map((s) => {
+        const languageDisplayText = localStore.localeDisplayOptions.find(
+            (l) => l.code === s
+        )?.simpleText
+        return languageDisplayText
+    })
 
-        return languagesDisplayText
-    }
-)
+    return languagesDisplayText
+})
 </script>
