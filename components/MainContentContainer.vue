9b8f99e07f3405b7ae2814b342feea4c80ef62bc
Sulaymane Sillah <sssillah1@gmail.com>
2023-12-18 22:28:38 +0900

    merged main into branch

--- components/MainContentContainer.vue
+++ components/MainContentContainer.vue
@@ -2,7 +2,8 @@
 import SearchResultDetails from './SearchResultDetails.vue';
 
 <template>
-    <div class="flex flex-1 bg-secondary-bg/20 hover:shadow-inner hover:shadow-secondary-bg/90">
+    <div
+        class="flex flex-1 bg-secondary-bg/20 hover:shadow-inner hover:shadow-secondary-bg/90 sm:flex sm:flex-col sm:w-full sm:order-1">
         <!-- <WelcomeSection /> -->
         <Loader />
         <Modal v-show="store.$state.isOpen" class="min-h-1/2 ml-8 mt-12">
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="content-container flex overflow-hidden sm:flex-col">
-        <div class="main-content-container flex-1 sm:flex-col">
+        <div class="main-content-container flex-1 sm:flex sm:flex-col">
             <BetaBanner />
             <div class="flex overflow-hidden sm:w-full">
-                <LeftNavbar class="flex-0 bg-primary-bg w-[358px] sm:w-full" />
-                <div class="flex-1 sm:order-1">
-                    <div class="flex flex-col md:flex-row h-full sm:flex-col">
+                <LeftNavbar class="flex-0 bg-primary-bg w-[358px] sm:w-full sm:order-2" />
+                <div class="flex-1 sm:-order-1">
+                    <div class="flex flex-col sm:w-full md:flex-row h-full">
                         <MainContentContainer />
                     </div>
                 </div>
@@ -13,16 +13,3 @@
         </div>
     </div>
 </template>
-<!--
-<style>
-@media screen and (max-width: 640px) {
-    .content-container {
-        flex-direction: column;
-    }
-
-    .left-container {
-        width: 100%;
-
-    }
-}
-</style> -->
