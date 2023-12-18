f18dd40b20dd1d18d6aee31e69551cca242d2dd5
Sulaymane Sillah <sssillah1@gmail.com>
2023-12-18 18:16:32 +0900

    committing changes to pull new branch

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,28 @@
 <template>
-    <div class="content-container flex overflow-hidden">
-        <LeftNavbar class="left-container sm:order-2 flex-0 bg-primary-bg w-[358px]" />
-        <div class="main-content-container sm:-order-1 flex-1">
-    <BetaBanner />
-    <div class="flex overflow-hidden">
-        <LeftNavbar class="flex-0 bg-primary-bg w-[358px]" />
-        <div class="flex-1">
-            <div class="flex flex-col md:flex-row h-full">
-                <MainContentContainer />
+    <div class="content-container flex overflow-hidden sm:flex-col">
+        <div class="main-content-container flex-1 sm:flex-col">
+            <BetaBanner />
+            <div class="flex overflow-hidden sm:w-full">
+                <LeftNavbar class="flex-0 bg-primary-bg w-[358px] sm:w-full" />
+                <div class="flex-1 sm:order-1">
+                    <div class="flex flex-col md:flex-row h-full sm:flex-col">
+                        <MainContentContainer />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
-
+<!--
 <style>
 @media screen and (max-width: 640px) {
-  .content-container {
-    flex-direction: column;
-  }
+    .content-container {
+        flex-direction: column;
+    }
 
-  .left-container {
-    width: 100%;
+    .left-container {
+        width: 100%;
 
-  }
+    }
 }
-</style>
+</style> -->
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
     content: [
-        './components/**/*.{js,vue,ts}',
-        './layouts/**/*.vue',
-        './pages/**/*.vue',
-        './plugins/**/*.{js,ts}',
-        './nuxt.config.{js,ts}',
-        './app.vue'
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+        "./app.vue",
     ],
     theme: {
         screens: {
-            md: { min: '800px' }
+            md: { min: "800px" },
+            sm: { max: "640px" },
         },
         colors: {
             ////from tailwind labs https://youtu.be/MAtaT8BZEAo. allows opacity to still work with css vars.
             //https://tailwindcss.com/docs/customizing-colors#using-css-variables
-            transparent: 'transparent',
-            current: 'currentColor',
-            currentColor: '#ED6C5A',
-            primary: 'rgb(var(--color-primary) / <alpha-value>)',
-            secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
-            tertiary: 'rgb(var(--color-tertiary) / <alpha-value>)',
-            'primary-inverted': 'rgb(var(--color-primary-inverted) / <alpha-value>)',
-            'primary-hover': 'rgb(var(--color-primary-hover) / <alpha-value>)',
-            'primary-bg': 'rgb(var(--color-bg-primary) / <alpha-value>)',
-            'secondary-bg': 'rgb(var(--color-bg-secondary) / <alpha-value>)',
-            'primary-text': 'rgb(var(--color-text-base) / <alpha-value>)',
-            'primary-text-muted': 'rgb(var(--color-text-muted) / <alpha-value>)',
-            'primary-text-inverted': 'rgb(var(--color-text-inverted) / <alpha-value>)',
-            'secondary-inverted': 'rgb(var(--color-secondary-inverted) / <alpha-value>)',
-            'secondary-hover': 'rgb(var(--color-secondary-hover) / <alpha-value>)',
-            blue: '#245A7D',
+            transparent: "transparent",
+            current: "currentColor",
+            currentColor: "#ED6C5A",
+            primary: "rgb(var(--color-primary) / <alpha-value>)",
+            secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+            tertiary: "rgb(var(--color-tertiary) / <alpha-value>)",
+            "primary-inverted":
+                "rgb(var(--color-primary-inverted) / <alpha-value>)",
+            "primary-hover": "rgb(var(--color-primary-hover) / <alpha-value>)",
+            "primary-bg": "rgb(var(--color-bg-primary) / <alpha-value>)",
+            "secondary-bg": "rgb(var(--color-bg-secondary) / <alpha-value>)",
+            "primary-text": "rgb(var(--color-text-base) / <alpha-value>)",
+            "primary-text-muted":
+                "rgb(var(--color-text-muted) / <alpha-value>)",
+            "primary-text-inverted":
+                "rgb(var(--color-text-inverted) / <alpha-value>)",
+            "secondary-inverted":
+                "rgb(var(--color-secondary-inverted) / <alpha-value>)",
+            "secondary-hover":
+                "rgb(var(--color-secondary-hover) / <alpha-value>)",
+            blue: "#245A7D",
             neutral: colors.gray,
             black: colors.black,
             white: colors.white,
@@ -48,20 +54,20 @@ module.exports = {
         // this section is temporary for the beta marquee banner
         extend: {
             animation: {
-                marquee: 'marquee 50s linear infinite',
-                marquee2: 'marquee2 50s linear infinite',
+                marquee: "marquee 50s linear infinite",
+                marquee2: "marquee2 50s linear infinite",
             },
             keyframes: {
                 marquee: {
-                    '0%': { transform: 'translateX(0%)' },
-                    '100%': { transform: 'translateX(-100%)' },
+                    "0%": { transform: "translateX(0%)" },
+                    "100%": { transform: "translateX(-100%)" },
                 },
                 marquee2: {
-                    '0%': { transform: 'translateX(100%)' },
-                    '100%': { transform: 'translateX(0%)' },
+                    "0%": { transform: "translateX(100%)" },
+                    "100%": { transform: "translateX(0%)" },
                 },
-            }
-        }
+            },
+        },
     },
-    plugins: []
-}
+    plugins: [],
+};
