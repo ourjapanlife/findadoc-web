122d9993fb7b6e4667d9e2e246eb6c79c04d8be0
theyokohamalife <lts001@gmail.com>
2023-12-31 04:45:09 +0900

    fix: contain root template in one element

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
-    <BetaBanner />
-    <div class="flex overflow-hidden">
-        <LeftNavbar class="flex-0 bg-primary-bg w-[358px]" />
-        <div class="flex-1">
-            <div class="flex flex-col md:flex-row h-full">
-                <MainContentContainer />
+    <div class="flex flex-col">
+        <BetaBanner />
+        <div class="flex overflow-hidden">
+            <LeftNavbar class="flex-0 bg-primary-bg w-[358px]" />
+            <div class="flex-1">
+                <div class="flex flex-col md:flex-row h-full">
+                    <MainContentContainer />
+                </div>
             </div>
         </div>
     </div>
