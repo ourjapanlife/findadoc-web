82f6622973d14fc3fc5673a4a7a631af26c93c50
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2023-12-31 10:42:02 +0900

    feat: add nuxt-viewport for breakpoints (#351)
    * feat: add nuxt-viewport for breakpoints
    
    * fix: add breakpoints to make site usable on mobile (#353)

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="flex flex-col">
         <BetaBanner />
-        <div class="flex overflow-hidden">
+        <div
+            v-if="$viewport.isGreaterThan('tablet')"
+            class="flex overflow-hidden"
+        >
             <LeftNavbar class="flex-0 bg-primary-bg w-[358px]" />
             <div class="flex-1">
                 <div class="flex flex-col md:flex-row h-full">
@@ -9,5 +12,20 @@
                 </div>
             </div>
         </div>
+        <div v-else>
+            <div class="flex-1">
+                <div class="flex flex-col h-full">
+                    <MainContentContainer />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script setup lang="ts">
+import { useNuxtApp } from "#app";
+import { useModalStore } from "~/stores/modalStore";
+
+const { $viewport } = useNuxtApp();
+const store = useModalStore();
+</script>
