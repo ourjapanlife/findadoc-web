6487e6c6d8644ea277f588e313a583c926fc3191
Philip Ermish <philipermish@gmail.com>
2024-03-14 21:53:36 +0900

    cleaned up added semicolons

--- utils/graphql.ts
+++ utils/graphql.ts
@@ -1,24 +1,24 @@
-import { GraphQLClient } from "graphql-request";
+import { GraphQLClient } from "graphql-request"
 
 export let gqlClient: GraphQLClient;
 
 export const initializeGqlClient = () => {
     if (gqlClient) {
-        return gqlClient;
+        return gqlClient
     }
 
-    const endpoint = "https://api.findadoc.jp";
-    const client = new GraphQLClient(endpoint);
-    gqlClient = client;
+    const endpoint = "https://api.findadoc.jp"
+    const client = new GraphQLClient(endpoint)
+    gqlClient = client
 };
 
 export interface gqlMutation<T> extends gqlRequest {
     variables: {
-        input: T;
-    };
+        input: T
+    }
 }
 
 export interface gqlRequest {
-    query: string;
-    variables: unknown;
+    query: string
+    variables: unknown
 }
