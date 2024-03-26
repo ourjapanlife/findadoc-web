cf0811ad906d61b349b31e496cccdb2fa6ea65ab
Will Brammer <williamedwardbrammer@gmail.com>
2024-03-26 17:18:59 +0900

    style: change TopNav Styling
    change the padding of the dropdown for language to be the same on desktop
    
    feat: implement vue-select as plugin
    
    I installed the vue-select package and was able to get it to show and work when testing in the search bar
    
    fix: add improt to plugin file

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,7 +89,7 @@ export default defineNuxtConfig({
     css: ['~/assets/css/tailwind.css'],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
+    plugins: ['~/plugins/vue-select.js'],
 
     // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
