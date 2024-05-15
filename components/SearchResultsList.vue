e10bc0c5038449455ef9e4b67264b3be6c64ae56
Sulaymane Sillah <99618731+Tch4lla@users.noreply.github.com>
2024-05-15 21:57:40 +0900

    add no results indicator (#462)
    * add no results indicator
    
    * made string translatable and removed template
    
    * added svg of no results image
    
    * updated conditional to computed property

--- components/SearchResultsList.vue
+++ components/SearchResultsList.vue
@@ -10,36 +10,48 @@
                     class="hamburger-list-icon fill-primary/50 h-5 w-5 self-center group-hover:fill-primary-text-inverted/50">
                     <use xlink:href="../assets/images/hamburger-list-icon.svg#hamburger-list-icon-svg" />
                 </svg>
-                <span class="text-primary-text/50 pl-1 font-bold self-center group-hover:text-primary-text-inverted/50">{{
-                    $t('searchResultsList.filters') }}</span>
+                <span class="text-primary-text/50 pl-1 font-bold self-center group-hover:text-primary-text-inverted/50">
+                    {{ $t('searchResultsList.filters') }}</span>
             </button>
         </div>
-        <div id="searchResults" class="flex flex-col overflow-y-scroll h-full">
-            <div @click="resultsStore.setActiveSearchResult(searchResult.professional.id)" :key="index"
-                v-for="(searchResult, index) in resultsStore.searchResultsList" class="results-list flex flex-col">
-                <div class="flex-1 h-24 w-6/8 border-b-2 border-primary/20 p-3
-                    hover:border-transparent hover:bg-primary/5 transition-all hover:cursor-pointer">
-                    <SearchResultsListItem
-                        :name="`${searchResult.professional.names[0].firstName} ${searchResult.professional.names[0].lastName}`"
-                        :degrees="searchResult.professional.degrees"
-                        :facility-name="localeStore.locale?.code == Locale.EnUs ? searchResult.facilities[0]?.nameEn : searchResult.facilities[0]?.nameJa"
-                        :specialties="searchResult.professional.specialties"
-                        :spoken-languages="searchResult.professional.spokenLanguages" />
+        <div v-if="hasResults">
+            <div id="searchResults" class="flex flex-col overflow-y-scroll h-full">
+                <div @click="resultsStore.setActiveSearchResult(searchResult.professional.id)" :key="index"
+                    v-for="(searchResult, index) in resultsStore.searchResultsList" class="results-list flex flex-col">
+                    <div class="flex-1 h-24 w-6/8 border-b-2 border-primary/20 p-3
+                        hover:border-transparent hover:bg-primary/5 transition-all hover:cursor-pointer">
+                        <SearchResultsListItem
+                            :name="`${searchResult.professional.names[0].firstName} ${searchResult.professional.names[0].lastName}`"
+                            :degrees="searchResult.professional.degrees"
+                            :facility-name="localeStore.locale?.code == Locale.EnUs ? searchResult.facilities[0]?.nameEn : searchResult.facilities[0]?.nameJa"
+                            :specialties="searchResult.professional.specialties"
+                            :spoken-languages="searchResult.professional.spokenLanguages" />
+                    </div>
                 </div>
+            </div>
+        </div>
+        <div v-else>
+            <div class="h-full flex flex-col w-[358px]">
+                <img :src='noSearchResultsGraphic' class="p-12" />
+                <span class="text-primary-text pl-1 font-bold self-center group-hover:text-primary-text-inverted/50">{{
+                    $t('searchResultsList.noResults') }}</span>
+                <span class="text-primary-text self-center">{{
+                    $t('searchResultsList.noResultsSubtext') }}</span>
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { useSearchResultsStore } from '../stores/searchResultsStore'
 import { useLocaleStore } from '../stores/localeStore'
 import { Locale } from '~/typedefs/gqlTypes.js';
-
+import noSearchResultsGraphic from '@/assets/images/no-search-results-graphic.svg'
 const resultsStore = useSearchResultsStore()
 const localeStore = useLocaleStore()
-
 //let's start with some initial data
 resultsStore.search()
+const hasResults = computed(() => resultsStore.searchResultsList.length > 0)
 
 </script>
