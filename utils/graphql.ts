db9f31c9314184c142ba8ed9a1e608ea0fded7d5
theyokohamalife <lts001@gmail.com>
2024-05-27 21:42:02 +0900

    feat: add cond for local server endpoint
    Co-authored-by: Anissa Chadouli <Anissa3005@users.noreply.github.com>

--- utils/graphql.ts
+++ utils/graphql.ts
@@ -1,24 +1,32 @@
-import { GraphQLClient } from "graphql-request"
+import { GraphQLClient } from "graphql-request";
 
-export let gqlClient: GraphQLClient;
+export let gqlClient: GraphQLClient
 
 export const initializeGqlClient = () => {
     if (gqlClient) {
         return gqlClient
     }
 
-    const endpoint = "https://api.findadoc.jp"
-    const client = new GraphQLClient(endpoint)
+    let apiURL
+
+    if (process.env.NODE_ENV == 'development') {
+        apiURL = "http://127.0.0.1:4000"
+
+    } else {
+        apiURL = "https://api.findadoc.jp"
+    }
+
+    const client = new GraphQLClient(apiURL)
     gqlClient = client
 };
 
 export interface gqlMutation<T> extends gqlRequest {
     variables: {
         input: T
-    }
+    };
 }
 
 export interface gqlRequest {
-    query: string
+    query: string;
     variables: unknown
 }
