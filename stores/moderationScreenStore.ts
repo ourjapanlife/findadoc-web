98c9af08189f6d4556b201733bbbed651e66e983
ShizuokaTerrier <matt@squareoneJapan.com>
2024-05-27 16:45:24 +0900

    feat: create topbar for edit submission form
    This change will implement the top bar for editing submission forms with partial functionality
    
    Co-authored-by: Ntlja <Ntlja@users.noreply.github.com>
    Co-authored-by: NabbeunNabi <williamedwardbrammer@gmail.com>
    Co-authored-by: Anissa Chadouli <Anissa3005@users.noreply.github.com>
    
    feat: implement basic click functionality
    
    style: updated style to look like figma

--- stores/moderationScreenStore.ts
+++ stores/moderationScreenStore.ts
@@ -9,14 +9,20 @@ export enum ModerationScreen {
 export const useModerationScreenStore = defineStore('moderationScreenStore', () => {
     const activeScreen: Ref<ModerationScreen> = ref(ModerationScreen.dashboard)
     const enableModerationPanel: Ref<boolean> = ref(false)
+    const fakeData = [
+        { "id": "13243214", "name": "Name 1", "status": "forReview", "modified": "Modified 1", "submitted": "Submitted 1" },
+        { "id": "252ewwe", "name": "Name 2", "status": "accepted", "modified": "Modified 2", "submitted": "Submitted 2" },
+        { "id": "3dfdanns", "name": "Name 3", "status": "rejected", "modified": "Modified 3", "submitted": "Submitted 3" }
+    ]
+    const selectedSubmissionId = ''
 
     function setActiveScreen(newValue: ModerationScreen) {
         activeScreen.value = newValue
     }
 
-    function setEnableModerationPanelToTrue ( currentPath:string){  
+    function setEnableModerationPanelToTrue(currentPath: string) {
         process.env.ENABLE_MODERATION_PANEL && currentPath.includes("moderation") ? enableModerationPanel.value = true : null
     }
 
-    return { activeScreen, enableModerationPanel, setActiveScreen, setEnableModerationPanelToTrue }
+    return { activeScreen, enableModerationPanel, fakeData, ModerationScreen, selectedSubmissionId, setActiveScreen, setEnableModerationPanelToTrue }
 })
