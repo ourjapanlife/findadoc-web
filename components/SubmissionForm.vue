c20d53325c10d8b9fa5d4567e88381cf54d6ee76
theyokohamalife <lts001@gmail.com>
2024-05-27 10:06:38 +0900

    test: add testing for submit page

--- components/SubmissionForm.vue
+++ components/SubmissionForm.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex flex-col place-self-center">
     <h1
-      class="mb-3.5 text-center text-zinc-900 text-2xl font-bold font-['Noto Sans JP'] leading-normal"
+        data-testid="submit-heading" class="mb-3.5 text-center text-zinc-900 text-2xl font-bold font-['Noto Sans JP'] leading-normal"
     >{{ $t('submitPage.heading')}}</h1>
     <p
-      class="mb-10 w-[350px] text-center text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
+        data-testid="submit-subheading" class="mb-10 w-[350px] text-center text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
     >{{ $t('submitPage.subheading')}}</p>
     <span
-      class="mb-2 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
+        class="mb-2 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
     >{{ $t('submitPage.googleMaps')}}</span>
-    <input
+    <input data-testid="submit-input-googlemaps"
       type="text"
       class="mb-5 px-3 py-3.5 w-[350px] h-[50px] bg-white rounded-lg border border-zinc-400 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
       :placeholder="$t('submitPage.location')"
@@ -19,13 +19,13 @@
       class="mb-2 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
     >{{ $t('submitPage.healthcareProfessionalName')}}</span>
     <div>
-      <input
+      <input data-testid="submit-input-lastname"
         class="mb-5 mr-2 px-3 py-3.5 w-[170px] h-[50px] bg-white rounded-lg border border-zinc-400 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
         type="text"
         :placeholder="$t('submitPage.lastName')"
         v-model="submissionStore.lastName"
       />
-      <input
+      <input data-testid="submit-input-firstname"
         class="mb-5 px-3 py-3.5 w-[170px] h-[50px] bg-white rounded-lg border border-zinc-400 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
         type="text"
         :placeholder="$t('submitPage.firstName')"
@@ -35,7 +35,7 @@
     <span
       class="mb-2 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
     >{{ $t('submitPage.spokenLanguage1')}}</span>
-    <select
+    <select data-testid="submit-select-language1"
       class="mb-5 px-3 py-3.5 w-[350px] h-[50px] bg-white rounded-lg border border-zinc-400 text-primary-text"
       :placeholder="$t('submitPage.selectLanguage1')"
       v-model="submissionStore.selectLanguage1"
@@ -51,6 +51,7 @@
       class="mb-2 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
     >{{ $t('submitPage.spokenLanguage2')}}</span>
     <select
+    data-testid="submit-select-language2"
       class="mb-5 px-3 py-3.5 w-[350px] h-[50px] bg-white rounded-lg border border-zinc-400 text-primary-text"
       :placeholder="$t('submitPage.selectLanguage2')"
       v-model="submissionStore.selectLanguage2"
@@ -66,11 +67,13 @@
       class="mb-2 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
     >{{ $t('submitPage.otherNotes')}}({{ $t('submitPage.optional')}})</span>
     <input
+    data-testid="submit-input-notes"
       class="mb-[71px] px-3 py-3.5 w-[350px] h-[50px] bg-white rounded-lg border border-zinc-400"
       type="text"
       v-model="submissionStore.otherNotes"
     />
     <button
+    data-testid="submit-submitbutton"
       class="px-20 py-3 rounded-full bg-currentColor w-[350px] text-center text-white text-base font-medium font-['Noto Sans JP']"
       @click="submissionStore.submit()"
     >{{ $t('submitPage.submitButton')}}</button>
---
+++ cypress/e2e/submit.cy.ts
@@ -0,0 +1,80 @@
+import enUS from '../../i18n/locales/en.json'
+
+describe('Submit page', () => {
+    context('Desktop resolution', () => {
+        beforeEach(() => {
+            cy.viewport(1920, 1080)
+            cy.visit('/submit')
+        })
+
+        it('shows the desktop nav', () => {
+            cy.get('[data-testid="desktop-nav"]').should('exist')
+        });
+
+        it('does not show the hamburger component', () => {
+            cy.get('[data-testid="hamburger-component"]').should('not.exist')
+        });
+
+        it('has a heading', () => {
+            cy.get('h1').should('have.attr', 'data-testid', 'submit-heading').contains(enUS.submitPage.heading)
+        });
+
+        it('has a subheading', () => {
+            cy.get('[data-testid="submit-subheading"]').contains(enUS.submitPage.subheading)
+        });
+
+        it('has an input field for Google Maps', () => {
+            cy.get('[data-testid="submit-input-googlemaps"]').should('have.match', 'input')
+        });
+
+        it('has an input field for last name', () => {
+            cy.get('[data-testid="submit-input-lastname"]').should('have.match', 'input')
+        });
+
+        it('has an input field for first name', () => {
+            cy.get('[data-testid="submit-input-firstname"]').should('have.match', 'input')
+        });
+
+        it('has a select field for language 1', () => {
+            cy.get('[data-testid="submit-select-language1"]').should('have.match', 'select')
+        });
+
+        it('has a select field for language 2', () => {
+            cy.get('[data-testid="submit-select-language2"]').should('have.match', 'select')
+        });
+
+        it('has an input field for notes', () => {
+            cy.get('[data-testid="submit-input-notes"]').should('have.match', 'input')
+        });
+
+        it('has a submit button', () => {
+            cy.get('[data-testid="submit-submitbutton"]').should('have.match', 'button')
+        });
+
+        it('shows the footer without scrolling', () => {
+            cy.isInViewport('[data-testid="footer"]')
+        });
+
+
+    })
+
+    context('iphone-5 resolution', () => {
+        beforeEach(() => {
+            cy.viewport('iphone-5')
+            cy.visit('/submit')
+        })
+
+        it('shows the hamburger component', () => {
+            cy.get('[data-testid="hamburger-component"]').should('exist')
+        });
+
+        it('does not show the desktop nav', () => {
+            cy.get('[data-testid="desktop-nav"]').should('not.exist')
+        });
+
+        it('does not show the footer', () => {
+            cy.get('[data-testid="footer"]').should('not.exist')
+        });
+
+    })
+})
