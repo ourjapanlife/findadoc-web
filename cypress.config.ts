d5b8a6de1083311a4069219e89ec96718368696b
theyokohamalife <lts001@gmail.com>
2024-05-28 21:29:27 +0900

    wip

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-    projectId: 'brkojt',
+    projectId: 'findadoc-web',
     e2e: {
         setupNodeEvents() { },
         baseUrl: 'http://localhost:3000',
-        testIsolation: false
+        testIsolation: false,
+        env: {
+            ENABLE_MODERATION_PANEL: true,
+        },
     },
     viewportHeight: 927,
     viewportWidth: 1600
