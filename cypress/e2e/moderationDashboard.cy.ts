aea363f6af06e5da1442ebf14d3004efedd69a03
NabbeunNabi <williamedwardbrammer@gmail.com>
2024-05-31 11:51:55 +0900

    refactor: change dynamic code to static
    This also moves the text into the i18n files
    
    feat: implement text into i18n files
    
    test: implement tests for mod dashboard navbar

--- cypress/e2e/moderationDashboard.cy.ts
+++ cypress/e2e/moderationDashboard.cy.ts
@@ -1,34 +1,65 @@
-import "cypress-real-events"
+import "cypress-real-events";
+import enUS from "../../i18n/locales/en.json";
 
-describe('Moderation dashboard', {
-    env: {
-        ENABLE_MODERATION_PANEL: true,
+describe(
+    "Moderation dashboard",
+    {
+        env: {
+            ENABLE_MODERATION_PANEL: true,
+        },
     },
-}, () => {
-    beforeEach(() => {
+    () => {
+        beforeEach(() => {}),
+            context("Desktop resolution", () => {
+                before(() => {
+                    cy.viewport(1920, 1080);
+                    cy.visit("/moderation");
+                });
 
-    }),
-        context('Desktop resolution', () => {
-            before(() => {
-                cy.viewport(1920, 1080)
-                cy.visit('/moderation')
-            })
+                it("shows mod dashboard left navbar buttons", () => {
+                    cy.wait(500);
 
-            it('it shows the moderation top nav', () => {
-                // wait for the vue components to actually load
-                cy.wait(500)
+                    cy.get("[data-testid=mod-dashboard-leftnav-for-review]")
+                        .should("exist")
+                        .should(
+                            "include.text",
+                            enUS.modDashboardLeftNav.forReview
+                        );
 
-                cy.get('[data-testid="mod-submission-list-item-1"]').click()
-                cy.get('[data-testid="mod-edit-submission-copy-submission-id"]').click()
+                    cy.get("[data-testid=mod-dashboard-leftnav-approved]")
+                        .should("exist")
+                        .should(
+                            "include.text",
+                            enUS.modDashboardLeftNav.approved
+                        );
 
-                // check that the value copied to the clipboard is the same that's displayed
-                const clipboardResult = cy.window().then((win) => {
-                    return win.navigator.clipboard.readText()
-                })
+                    cy.get("[data-testid=mod-dashboard-leftnav-rejected]")
+                        .should("exist")
+                        .should(
+                            "include.text",
+                            enUS.modDashboardLeftNav.rejected
+                        );
+                });
 
-                // the timeout is to give time for the clipboard to be read
-                clipboardResult.should('exist', 10000)
-            })
+                it("it shows the moderation top nav", () => {
+                    // wait for the vue components to actually load
+                    cy.wait(500);
 
-        })
-})
+                    cy.get(
+                        '[data-testid="mod-submission-list-item-1"]'
+                    ).click();
+                    cy.get(
+                        '[data-testid="mod-edit-submission-copy-submission-id"]'
+                    ).click();
+
+                    // check that the value copied to the clipboard is the same that's displayed
+                    const clipboardResult = cy.window().then((win) => {
+                        return win.navigator.clipboard.readText();
+                    });
+
+                    // the timeout is to give time for the clipboard to be read
+                    clipboardResult.should("exist", 10000);
+                });
+            });
+    }
+);
