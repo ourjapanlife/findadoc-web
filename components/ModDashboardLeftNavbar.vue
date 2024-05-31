872f03667fa85439a77e394beaae41bed5dbf8c0
NabbeunNabi <williamedwardbrammer@gmail.com>
2024-05-31 11:23:44 +0900

    refactor: change dynamic to static
    In this change I also moved the text which was dynamically generated to the text.json i18n files

--- components/ModDashboardLeftNavbar.vue
+++ components/ModDashboardLeftNavbar.vue
@@ -1,61 +1,47 @@
 <template>
     <div>
-        <select v-model="selectedDashboardView" class="font-bold border-b border-black mb-2">
-            <option :key="dashboardViewOption.value" :value="dashboardViewOption.value"
-                v-for="(dashboardViewOption, index) in modDashboardViewOptions">
-                {{ $t(dashboardViewOption.displayText) }}
+        <select class="font-bold border-b border-black mb-2">
+            <option value="facilities">
+                {{ $t("modDashboardLeftNav.facilities") }}
+            </option>
+            <option value="healthcareProfessionals">
+                {{ $t("modDashboardLeftNav.healthcareProfessionals") }}
             </option>
         </select>
         <div class="flex flex-col">
-            <button @click="updateSubmissionListViewState(submissionListEntry.value)"
-                class="flex flex-row items-center text-start p-1" :key="submissionListEntry.value"
-                :class="{ 'border-b border-gray-300': index !== submissionListViewOptions.length - 1 }"
-                v-for="(submissionListEntry, index) in submissionListViewOptions ">
-                <div class="flex items-center justify-center w-1/5">
-                    <component :is="submissionListEntry.svgIcon" class="h-5" />
-                </div>
-                {{ $t(submissionListEntry.displayText) }} ({{ submissionListEntry.count }})
-            </button>
+            <div class="flex flex-row w-4/5 justify-start items-center border-b-2 border-slate-200">
+                <SVGNoteStackAddSvg class="h-4" />
+                <button @click="updateSubmissionListViewState('forReview')"
+                    class="flex flex-row items-center text-start p-1">
+                    {{ $t("modDashboardLeftNav.forReview") }} ({{ 0 }})
+                </button>
+            </div>
+            <div class="flex flex-row w-4/5 justify-start items-center border-b-2 border-slate-200">
+                <SVGCheckBoxSvg class="h-5" />
+                <button @click="updateSubmissionListViewState('approved')"
+                    class="flex flex-row items-center text-start p-1 ">
+                    {{ $t("modDashboardLeftNav.approved") }} ({{ 0 }})
+                </button>
+            </div>
+            <div class="flex flex-row w-4/5 justify-start items-center ">
+                <SVGDisabledByDefault class="h-4" />
+                <button @click="updateSubmissionListViewState('rejected')"
+                    class="flex flex-row items-center text-start p-1">
+                    {{ $t("modDashboardLeftNav.rejected") }} ({{ 0 }})
+                </button>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref, Ref } from 'vue'
-import NoteStackAddSvg from "../assets/icons/note-stack-add.svg"
-import CheckBoxSvg from "../assets/icons/check-box.svg"
-import DisabledByDefault from "../assets/icons/disabled-by-default.svg"
+import SVGNoteStackAddSvg from "../assets/icons/note-stack-add.svg"
+import SVGCheckBoxSvg from "../assets/icons/check-box.svg"
+import SVGDisabledByDefault from "../assets/icons/disabled-by-default.svg"
 
-const modDashboardViewOptions = [{
-    displayText: "Facilities",
-    value: "facilities"
-},
-{
-    displayText: "Healthcare Professionals",
-    value: "healthcareProfessionals"
-}]
-
-const submissionListViewOptions = [{
-    displayText: "For Review",
-    value: "forReview",
-    svgIcon: NoteStackAddSvg,
-    count: 0,
-},
-{
-    displayText: "Approved",
-    value: "approved",
-    svgIcon: CheckBoxSvg,
-    count: 0,
-},
-{
-    displayText: "Rejected",
-    value: "rejected",
-    svgIcon: DisabledByDefault,
-    count: 0,
-}]
-
-const selectedSubmissionListView: Ref<String> = ref(submissionListViewOptions[0].value)
-const selectedDashboardView: Ref<String> = ref(modDashboardViewOptions[0].value)
+const selectedSubmissionListView: Ref<string> = ref("forReview")
+const selectedDashboardView: Ref<string> = ref("facilities")
 
 const updateSubmissionListViewState = (submissionListViewOptionValue: string) => {
     selectedSubmissionListView.value = submissionListViewOptionValue;
