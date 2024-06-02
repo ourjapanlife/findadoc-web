15f47e34b27d469fa3eb60e3548227c2660786bc
Philip Ermish <philipermish@gmail.com>
2024-06-03 00:16:45 +0900

    feat: built out hamburger menu and reworked topnav

--- components/HamburgerMenu.vue
+++ components/HamburgerMenu.vue
@@ -1,76 +1,94 @@
 <template>
     <div class="cursor-pointer w-full flex" v-close-on-outside-click>
-        <img id="hamburger-menu-icon" @click="menuStore.toggleMenu()" src="../assets/images/hamburger-menu.svg"
-            alt="hamburger menu" />
-        <div id="hamburger-menu" v-show="menuStore.isMenuOpen"
-            class="z-20 absolute top-0 left-0 h-full w-2/3 py-8 rounded bg-primary-bg border-2 flex flex-col justify-between">
+        <SVGHamburgerMenuIcon id="hamburger-menu-icon" @click="toggleMenu()" alt="hamburger menu" class="h-8 w-8" />
+        <div id="hamburger-menu" v-show="isMenuOpen"
+            class="z-20 absolute top-0 left-0 h-full w-2/3 pt-6 pb-8 rounded bg-primary-bg border-2 flex flex-col justify-between">
             <div id="upper-section">
-                <div id="header" class="flex flex-row pt-3 px-2">
-                    <button id="close-button" @click="menuStore.closeMenu()" class="bg-primary-inverted rounded-lg px-2 py-.5 mr-2
-                group hover:bg-primary-hover transition-all duration-200">
-                        <span class="close-icon">
-                            <svg class="stroke-primary group-hover:stroke-primary-inverted" width="20" heigh="20"
-                                viewBox="4 0 15 25">
-                                <path stroke-width="3" fill="none" d="M6.25,6.25,17.75,17.75" />
-                                <path stroke-width="3" fill="none" d="M6.25,17.75,17.75,6.25" />
-                            </svg>
-                        </span>
-                    </button>
+                <div id="header" class="flex justify-between px-5">
                     <div class="flex">
                         <div class="title-text flex flex-col flex-shrink-0" data-testid="logo">
-                            <div class="text-lg text-primary group-hover:text-primary-hover">
+                            <div class="text-lg text-primary-text group-hover:text-primary-hover font-semibold">
                                 Find a Doc, Japan
                             </div>
                         </div>
                     </div>
+                    <button id="close-button" @click="closeMenu()" class="px-2 py-.5 mr-2">
+                        <svg class="stroke-primary" width="20" heigh="20" viewBox="4 0 15 25">
+                            <path stroke-width="3" fill="none" d="M6.25,6.25,17.75,17.75" />
+                            <path stroke-width="3" fill="none" d="M6.25,17.75,17.75,6.25" />
+                        </svg>
+                    </button>
                 </div>
-                <div id="menu-items" class="mt-10 px-5">
-                    <div id="menu-item" class="my-8" v-for="(item, index) in menuStore.hamburgerMenuItems" :key="index">
-                        <NuxtLink :to="item.route">
-                            <span @click="menuStore.toggleMenu()"
-                                class="block hover:-translate-y-1 hover:scale-110 duration-300">
-                                {{ $t(item.displayText) }}
-                            </span>
-                        </NuxtLink>
-                    </div>
+                <div id="language-section" class="flex mt-2 px-5">
+                    <span class="text-primary-text self-center mr-2">{{ $t('hamburgerMenu.languageDropdownTitle') }}</span>
+                    <LocaleSelector class="py-1.5 text-primary-text bg-primary-bg" />
+                </div>
+                <div id="menu-items" class="mt-10 px-5 flex flex-col gap-6">
+                    <NuxtLink :to="'/'">
+                        <div @click="toggleMenu()">
+                            {{ $t('hamburgerMenu.home') }}
+                        </div>
+                    </NuxtLink>
+                    <NuxtLink :to="'/about'">
+                        <div @click="toggleMenu()">
+                            {{ $t('hamburgerMenu.about') }}
+                        </div>
+                    </NuxtLink>
+                    <NuxtLink :to="'https://forms.gle/4E763qfaq46kEsn99'" target="_blank">
+                        <div @click="toggleMenu()">
+                            {{ $t('hamburgerMenu.contact') }}
+                        </div>
+                    </NuxtLink>
+                    <NuxtLink :to="'/submit'">
+                        <div @click="toggleMenu()">
+                            {{ $t('hamburgerMenu.submit') }}
+                        </div>
+                    </NuxtLink>
                 </div>
             </div>
-            <div id="language-section" class="flex flex-col justify-between mt-4 px-5">
-                <span class="text-primary-text pl-1 my-1">{{ $t('hamburgerMenu.languageDropdownTitle') }}</span>
-                <LocaleSelector class="w-full py-1.5 text-primary-text bg-primary-bg transition-all" />
-                <!-- <select v-model="selectedLanguage"
-                    class="rounded-r-md rounded-l-md w-full px-1 border-2 border-primary/60 py-1.5
-                                drop-shadow-md text-primary-text bg-primary-bg hover:bg-primary-hover/10 transition-all"
-                    data-testid="search-bar-language">
-                    <option value="" class="text-primary-text-muted hidden" disabled selected>
-                        {{ $t('searchBar.selectLanguage') }}
-                    </option>
-                    <option :key="language.code" :value="language.code"
-                        v-for="(language, index) in languageDropdownOptions">
-                        {{ language.simpleText }}
-                    </option>
-                </select> -->
+
+            <div id="footer-section" class="flex flex-col gap-5 px-5">
+                <div id="row-1" class="flex gap-4">
+                    <NuxtLink :to="'/terms'">
+                        <span @click="toggleMenu()">
+                            {{ $t('footer.terms') }}
+                        </span>
+                    </NuxtLink>
+                    <NuxtLink :to="'/privacypolicy'">
+                        <span @click="toggleMenu()">
+                            {{ $t('footer.privacy') }}
+                        </span>
+                    </NuxtLink>
+                </div>
+                <div id="row-2" class="flex gap-6">
+                    <NuxtLink :to="'https://github.com/ourjapanlife'" target="_blank">
+                        <SVGGithubIcon id="github-icon" alt="hamburger menu" class="h-10 w-8" />
+                    </NuxtLink>
+                    <!-- Netlify Icons are available here: https://www.netlify.com/press/#badges -->
+                    <NuxtLink :to="'https://www.netlify.com'" target="_blank">
+                        <img src="https://www.netlify.com/v3/img/components/netlify-light.svg" alt="Deploys by Netlify"
+                            class=" h-10 w-20" />
+                    </NuxtLink>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-// import { ref, Ref } from 'vue'
-// import { LocaleDisplay, useLocaleStore } from '~/stores/localeStore'
-import { useMenuStore } from "~/stores/menuStore"
-// import { Locale } from "~/typedefs/gqlTypes"
+import SVGHamburgerMenuIcon from '~/assets/icons/hamburger-menu.svg'
+import SVGGithubIcon from '~/assets/icons/social-github.svg'
+import { ref } from 'vue'
 
-const menuStore = useMenuStore()
-// const localeStore = useLocaleStore()
+const isMenuOpen = ref(false)
 
-// const selectedLanguage: Ref<Locale | String> = ref('')
-// const languageOptions = [{
-//     code: '',
-//     simpleText: '----Any----',
-//     displayText: '----Any----'
-// }, ...localeStore.mvpLocaleDisplayOptions]
-// const languageDropdownOptions: Ref<LocaleDisplay[]> = ref(languageOptions)
+function toggleMenu() {
+    isMenuOpen.value = !isMenuOpen.value
+}
+
+function closeMenu() {
+    isMenuOpen.value = false
+}
 
 //this is a custom directive to close the menu when clicking outside of the menu
 const vCloseOnOutsideClick = {
@@ -78,7 +96,7 @@ const vCloseOnOutsideClick = {
         el.clickOutsideEvent = (event: any) => {
             const isClickInsideElement = (el == event.target || el.contains(event.target))
             if (!isClickInsideElement) {
-                menuStore.closeMenu()
+                closeMenu()
             }
         };
         document.addEventListener("click", el.clickOutsideEvent)
--- components/TopNav.vue
+++ components/TopNav.vue
@@ -1,71 +1,52 @@
 <template>
     <div
         class="p-4 pl-5 border border-secondary-bg/20 flex justify-between items-center bg-gradient-to-t from-secondary-bg/30 via-primary-bg to-primary-bg">
-        <div class="flex items-center">
-            <HamburgerMenu class="landscape:hidden" />
-            <div id="mobile-site-icon"
-                class="landscape:hidden font-semibold text-xl group transition-colors flex justify-between items-start">
-                <svg role="img" title="site icon"
-                    class="mr-1 w-10 h-10 flex-shrink-0 align-middle fill-primary group-hover:fill-primary-hover">
-                    <use xlink:href="../assets/images/site-logo.svg#site-logo-svg" />
-                </svg>
+        <div id="mobile-site-icon"
+            class="landscape:hidden font-semibold text-xl group transition-colors flex justify-between items-start">
+            <SVGSiteLogo role="img" title="site icon"
+                class="mt-1 mr-1 w-10 h-10 flex-shrink-0 align-middle fill-primary group-hover:fill-primary-hover" />
+            <div class="title-text flex flex-col flex-shrink-0" data-testid="logo">
+                <div class="text-lg text-primary group-hover:text-primary-hover">
+                    Find a Doc,
+                </div>
+                <div class="text-sm text-primary leading-none group-hover:text-primary-hover">
+                    Japan
+                </div>
+            </div>
+        </div>
+        <div id="desktop-site-icon" class="portrait:hidden font-semibold text-xl group transition-colors items-start">
+            <NuxtLink class="flex" to="/">
+                <SVGSiteLogo role="img" title="site icon"
+                    class="mr-1 w-10 h-10 flex-shrink-0 align-middle fill-primary group-hover:fill-primary-hover" />
                 <div class="title-text flex flex-col flex-shrink-0" data-testid="logo">
                     <div class="text-lg text-primary group-hover:text-primary-hover">
-                        Find a Doc,
+                        Find a Doc
                     </div>
                     <div class="text-sm text-primary leading-none group-hover:text-primary-hover">
                         Japan
                     </div>
                 </div>
-            </div>
-            <div id="desktop-site-icon"
-                class="portrait:hidden font-semibold text-xl group transition-colors items-start">
-                <NuxtLink class="flex" to="/">
-                    <svg role="img" title="site icon"
-                        class="mr-1 w-10 h-10 flex-shrink-0 align-middle fill-primary group-hover:fill-primary-hover">
-                        <use xlink:href="../assets/images/site-logo.svg#site-logo-svg" />
-                    </svg>
-                    <div class="title-text flex flex-col flex-shrink-0" data-testid="logo">
-                        <div class="text-lg text-primary group-hover:text-primary-hover">
-                   
-                            Find a Doc
-                        </div>
-                        <div class="text-sm text-primary leading-none group-hover:text-primary-hover">
-                            Japan
-                        </div>
-                    </div>
+            </NuxtLink>
+        </div>
+        <div id="desktop-searchbar" class="portrait:hidden flex align-middle mr-32">
+            <SearchBar />
+        </div>
+        <div id="right-section" class="flex">
+            <nav id="desktop-menu-items" class="portrait:hidden flex gap-4 mx-6 self-center">
+                <NuxtLink :to="'/about'" class="hover:text-primary-hover transition-colors">{{ $t('topNav.about') }}
                 </NuxtLink>
-            </div>
-            <div id="desktop-searchbar" class="portrait:hidden align-middle mr-32">
-                <SearchBar />
-            </div>
-            <nav id="desktop-menu-items" class="portrait:hidden flex gap-4 mx-6"
-                v-for="(item, index) in menuStore.menuItems" :key="index">
-                <NuxtLink :to="item.route" class="hover:text-primary-hover transition-colors">{{ $t(item.displayText) }}
+                <NuxtLink :to="'/'" class="hover:text-primary-hover transition-colors">{{ $t('topNav.home') }}
+                </NuxtLink>
+                <NuxtLink :to="'/submit'" class="hover:text-primary-hover transition-colors">{{ $t('topNav.submit') }}
                 </NuxtLink>
             </nav>
             <LocaleSelector class="portrait:hidden" />
-            <div id="mobile-search-button" class="landscape:hidden absolute right-5">
-                <button id="mobileSearchButton">
-                    <svg role="img" alt="search icon" title="search icon"
-                        class="search-icon w-8 h-8 mr-1 fill-primary-text-inverted">
-                        <use xlink:href="../assets/images/search-icon.svg#search-icon-svg" />
-                    </svg>
-                </button>
-            </div>
+            <HamburgerMenu class="landscape:hidden justify-end" />
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
 import SVGSiteLogo from '~/assets/icons/site-logo.svg'
-import { useSubmissionStore } from "~/stores/submissionStore";
-import { useMenuStore } from "~/stores/menuStore";
-import { useNuxtApp } from "#app";
-import HamburgerMenu from "./HamburgerMenu.vue";
-
-
-const { $viewport } = useNuxtApp()
-
-const menuStore = useMenuStore()
+import HamburgerMenu from "./HamburgerMenu.vue"
 </script>
