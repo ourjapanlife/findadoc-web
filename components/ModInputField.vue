f39ae90411d47bae2b98c6155fbd776cda6e2008
Anissa3005 <anissa.chadouli@gmail.com>
2024-06-11 14:00:16 +0900

    fix: Replace hard-coded text, colors, fonts and measurements
    - Replaced the hard-coded texts with i18
    - Replaced hard-coded colors, fonts and measurements with our themes

--- components/ModInputField.vue
+++ components/ModInputField.vue
@@ -1,9 +1,9 @@
 <template>
         <div class="flex flex-col mt-4">
-            <label class="mb-2 text-zinc-900 text-sm font-bold font-['Noto Sans JP']">
+            <label class="mb-2 text-primary-text text-sm font-bold font-sans">
                 {{ label }}
             </label>
-            <p v-if="!isTheInputValueValid" class="text-currentColor text-xs font-['Noto Sans JP']">{{ invalidInputErrorMessage }}</p>
+            <p v-if="!isTheInputValueValid" class="text-currentColor text-xs font-sans">{{ invalidInputErrorMessage }}</p>
             <input
                 @blur="() => checkValidationOfInputField(inputValue)"
                 :type='type'
