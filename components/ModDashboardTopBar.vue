c0a51b11a5bdac136f8e77ba072e6d4b1e6d2594
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2024-06-13 10:23:54 +0900

    Create TopBar comp dashboard. Need search logic
    - Created a component ModDashboardTopBar to hold the top bar when the user access the dashboard first time
    
    - The input element, wrapped in a form tag,  triggers by pressing enter and clicking in the magnifying glass icon. For now, it only console.logs the input's value.
    
    - Add the p content for english and non-translated string in japanese.
    
    - Add some padding to the left bar to look closer to the figma. I can revert it if I shouldn't have done that.

---
+++ components/ModDashboardTopBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1 class="text-lg font-bold flex items-center">{{ $t('modDashboardTopBar.forReview')}}</h1>
+            <div class="relative">
+                <form @submit.prevent="submitSearch">
+                <input @input="event => searchInput = event.target.value"
+                    type="text"
+                    class="rounded-lg border  border-slate-400 px-3 py-4 placeholder:text-black placeholder:text-sm placeholder:font-normal w-96 items-center"
+                    :placeholder="$t('searchBar.search')" />
+                <MagnifyingGlass @click="submitSearch" class="absolute right-3 top-1/2 -translate-y-1/2 w-6 h-6" />
+                </form>
+            </div>
+</template>
+
+<script setup lang="ts">
+import { Ref, ref } from 'vue';
+import MagnifyingGlass from '../assets/icons/magnifying-glass.svg';
+const searchInput: Ref<string> = ref("")
+
+const submitSearch = () => {
+    console.log(searchInput.value)
+    //logic to trigger the search
+
+}
+
+</script>
--- components/ModTopbar.vue
+++ components/ModTopbar.vue
@@ -2,11 +2,7 @@
     <!-- styling will change from px to rm -->
     <div class="h-[76px] w-full">
         <div class="flex px-6 py-3 justify-between w-full" v-if="store.activeScreen === ModerationScreen.dashboard">
-            <h1 class="text-lg font-bold flex items-center">For Review</h1>
-            <div class="relative">
-                <input type="text" class="rounded-lg border border-[#AEB0B5] px-3 py-4 placeholder:text-black placeholder:text-sm placeholder:font-normal w-96 items-center" placeholder="Search" />
-                <MagnifyingGlass class="absolute right-3 top-1/2 -translate-y-1/2 w-6 h-6" />
-            </div>
+            <ModDashboardTopBar data-test-id="mod-dashboard-topbar" />
         </div>
         <div class="h-[76px] w-full" v-else-if="store.activeScreen === ModerationScreen.editSubmission">
             <ModEditSubmissionTopbar data-test-id="mod-edit-submission-topbar" submissionId="" />
@@ -17,7 +13,7 @@
 <script setup lang="ts">
 import { useModerationScreenStore, ModerationScreen } from '~/stores/moderationScreenStore'
 import ModEditSubmissionTopbar from './ModEditSubmissionTopbar.vue';
-import MagnifyingGlass from '../assets/icons/magnifying-glass.svg';
+import ModDashboardTopBar from './ModDashboardTopBar.vue';
 
 
 const store = useModerationScreenStore()
