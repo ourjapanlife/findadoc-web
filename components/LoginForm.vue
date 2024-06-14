20220054d389df4f7a9c8415616786114183a284
Philip Ermish <philipermish@gmail.com>
2024-06-14 22:16:05 +0900

    code review fixes. moving string to i18n

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -3,7 +3,7 @@
         class="mt-16 mb-16 text-primary-text text-2xl font-bold flex self-center justify-items-center justify-center text-center w-full h-full">
         <SVGLoadingIcon role="img" alt="loading animation" title="loading animation" class="h-8" />
         <span>
-            Redirecting to login page...
+            {{ $t('login.redirectingtoauth0') }}...
         </span>
     </h1>
 </template>
