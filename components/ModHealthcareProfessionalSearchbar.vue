07664e274d39be415d0cb9b8e83ef4cd5f687dcc
Anissa3005 <anissa.chadouli@gmail.com>
2024-06-14 20:51:33 +0900

    style: change searchbar font class and size

--- components/ModHealthcareProfessionalSearchbar.vue
+++ components/ModHealthcareProfessionalSearchbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <span class="mb-3.5 text-center text-primary-text text-2xl font-bold font-['Noto Sans JP'] leading-normal">
+        <span class="mb-3.5 text-center text-primary-text text-2xl font-bold font-sans leading-normal">
             {{ $t('modSubmissionForm.healthcareProfessionalId') }}
         </span>
         <div class="flex flex-col mt-4">
@@ -8,7 +8,7 @@
                 <input 
                     type='text'
                     :placeholder="$t('modSubmissionForm.placeholderTextHealthcareProfessionalSearchbar')"
-                    class="mb-5 px-3 py-3.5 w-96 h-12 bg-white rounded-lg border border-primary-text-muted text-primary-text text-sm font-normal font-['Noto Sans JP'] placeholder-primary-text-muted"
+                    class="mb-5 px-3 py-3.5 w-96 h-12 bg-white rounded-lg border border-primary-text-muted text-primary-text text-sm font-normal font-sans placeholder-primary-text-muted"
                 />
                 <SVGLookingGlass role="img" title="searching icon" class="relative right-8 top-3 w-6 h-6"/>
             </div>
