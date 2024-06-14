8ba9e8f2938f5ebcde8f43c7f9f9fc0634daf383
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2024-06-14 12:37:17 +0900

    Added the portuguese json file and add the option
    - Added the portuguese json file with all text in portuguese.
    
    - Add the portuguese json file in the index.ts file to export it.
    
    - Added the portuguese option in the mvpLocaleDisplayOptions type, in the localeStore.
    
    - Changed from "Portuguese" to "Português" and switched from "Brazil" to "Brasil" in the localeDisplayOptions type in the store.

--- stores/localeStore.ts
+++ stores/localeStore.ts
@@ -64,7 +64,7 @@ export const localeDisplayOptions = [
     { code: Locale.NeNp, simpleText: 'Nepali', displayText: 'Nepali (Nepal)' },
     { code: Locale.NlBe, simpleText: 'Dutch', displayText: 'Dutch (Belgium)' },
     { code: Locale.PlPl, simpleText: 'Polish', displayText: 'Polish (Poland)' },
-    { code: Locale.PtBr, simpleText: 'Portuguese', displayText: 'Portuguese (Brazil)' },
+    { code: Locale.PtBr, simpleText: 'Português', displayText: 'Português (Brasil)' },
     { code: Locale.RuRu, simpleText: 'Russian', displayText: 'Russian (Russia)' },
     { code: Locale.SiLk, simpleText: 'Sinhala', displayText: 'Sinhala (Sri Lanka)' },
     { code: Locale.SqAl, simpleText: 'Albanian', displayText: 'Albanian (Albania)' },
@@ -82,4 +82,5 @@ export const mvpLocaleDisplayOptions = [
     { code: Locale.EnUs, simpleText: 'English', displayText: 'English (US)' },
     { code: Locale.JaJp, simpleText: '日本語', displayText: '日本語 (Japan)' },
     { code: Locale.SwKe, simpleText: 'Swahili', displayText: 'Swahili (Kenya)' },
+    { code: Locale.PtBr, simpleText: 'Português', displayText: 'Português (Brasil)' }
 ] satisfies LocaleDisplay[]
