30ed5ca3189668832e9c6f0b283fab828007bf06
William Brammer <williamedwardbrammer@gmail.com>
2024-06-15 11:52:01 +0900

    chore: update all the typescript types file by file (#568)

--- stores/loadingStore.ts
+++ stores/loadingStore.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { Ref, ref } from 'vue'
+import { ref, type Ref } from 'vue'
 
 export const useLoadingStore = defineStore('loading', () => {
     const isLoading: Ref<boolean> = ref(false)
