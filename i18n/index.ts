ef573787fb4261bf079073273ddae54e39b877d0
Diego Bittencourt Mendes <92993129+Diego-Bittencourt@users.noreply.github.com>
2024-06-18 17:52:55 +0900

    Added the portuguese json file and add the option (#564)
    - Added the portuguese json file with all text in portuguese.
    
    - Add the portuguese json file in the index.ts file to export it.
    
    - Added the portuguese option in the mvpLocaleDisplayOptions type, in the localeStore.
    
    - Changed from "Portuguese" to "Português" and switched from "Brazil" to "Brasil" in the localeDisplayOptions type in the store.

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,10 +1,12 @@
 import en from './locales/en.json'
 import ja from './locales/ja.json'
+import pt from './locales/pt.json'
 
 
 const i18n = {
     'en_US': en,
-    'ja_JP': ja
+    'ja_JP': ja,
+    'pt_BR': pt,
 }
 
 export default i18n
