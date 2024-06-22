77dd110b4a72c3cd77d95ebed21da4fcffb87654
Anissa3005 <anissa.chadouli@gmail.com>
2024-06-22 11:17:59 +0900

    Resolve merging conflicts

--- components/LocaleSelector.vue
+++ components/LocaleSelector.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="w-24">
-    <select
-      v-model="selectedLocale"
-      class="rounded-full landscape:w-full px-2 py-1 landscape:px-1 landscape:py-1.5 border-2 border-primary/80
+    <div class="w-24">
+        <select
+            v-model="selectedLocale"
+            class="rounded-full landscape:w-full px-2 py-1 landscape:px-1 landscape:py-1.5 border-2 border-primary/80
       landscape:border-primary/60 drop-shadow-md text-primary-text bg-secondary-bg/5
       landscape:bg-primary-bg hover:bg-primary-hover/10 transition-colors"
-    >
-      <option
-        v-for="(localeOption) in localeStore.mvpLocaleDisplayOptions"
-        :key="localeOption.code"
-        :value="localeOption.code"
-      >
-        {{ localeOption.simpleText }}
-      </option>
-    </select>
-  </div>
+        >
+            <option
+                v-for="(localeOption) in localeStore.mvpLocaleDisplayOptions"
+                :key="localeOption.code"
+                :value="localeOption.code"
+            >
+                {{ localeOption.simpleText }}
+            </option>
+        </select>
+    </div>
 </template>
 
 <script setup lang="ts">
