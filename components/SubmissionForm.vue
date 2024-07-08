016f042ea4e5a682453a81d6eeaa0dae11ebcf27
Anissa Chadouli <anissa.chadouli@gmail.com>
2024-07-08 18:07:03 +0900

    FIX: Change styling values using our Tailwind classes (#626)
    * fix: changed the styling class values
    
    - Before some styling classes contained hard coded colors, I have changed them to our set theme colors
    - Before some styling classes contained px values I have changed them to the Tailwind standards
    
    * fixup! fix: changed the styling class values

--- components/SubmissionForm.vue
+++ components/SubmissionForm.vue
@@ -2,22 +2,22 @@
     <form class="flex flex-col place-self-center">
         <h1
             data-testid="submit-heading"
-            class="mb-3.5 text-center text-zinc-900 text-2xl font-bold font-['Noto Sans JP'] leading-normal"
+            class="mb-3.5 text-center text-primary-text text-2xl font-bold font-sans leading-normal"
         >
             {{ $t('submitPage.heading') }}
         </h1>
         <p
             data-testid="submit-subheading"
-            class="mb-10 w-[350px] text-center text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
+            class="mb-10 w-96 text-center text-primary-text-muted text-sm font-normal font-sans"
         >
             {{ $t('submitPage.subheading') }}
         </p>
         <span
-            class="mb-2 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
+            class="mb-2 text-primary-text text-sm font-normal font-sans"
         >{{ $t('submitPage.googleMaps') }}</span>
         <p
             v-show="!isValidInput.googleMapsUrl.value"
-            class="text-currentColor text-xs font-['Noto Sans JP']"
+            class="text-error text-xs font-sans"
         >
             {{ $t('submitPage.googleMapsValidation') }}
         </p>
@@ -26,15 +26,15 @@
             data-testid="submit-input-googlemaps"
             type="text"
             required
-            class="mb-5 px-3 py-3.5 w-[350px] h-[50px] bg-white rounded-lg border border-zinc-400
-            text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
+            class="mb-5 px-3 py-3.5 w-96 h-12 bg-secondary-bg rounded-lg border border-primary-text-muted
+            text-primary-text text-sm font-normal font-sans placeholder-primary-text-muted"
             :placeholder="$t('submitPage.location')"
             @blur="initialValidationCheck(submissionStore.location, 'googleMaps')"
         >
         <span
-            class="mb-2 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
+            class="mb-2 text-primary-text text-sm font-normal font-sans"
         >{{ $t('submitPage.healthcareProfessionalName') }}</span>
-        <p class="flex text-currentColor text-xs font-['Noto Sans JP']">
+        <p class="flex text-error text-xs font-sans">
             <span
                 :class="[
                     !isValidInput.lastName.value ? 'visible' : 'invisible',
@@ -57,8 +57,8 @@
             <input
                 v-model="submissionStore.lastName"
                 data-testid="submit-input-lastname"
-                class="mb-5 mr-2 px-3 py-3.5 w-[170px] h-[50px] bg-white rounded-lg border border-zinc-400
-            text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
+                class="mb-5 mr-1 px-3 py-3.5 w-48 h-12 bg-secondary-bg rounded-lg border border-primary-text-muted
+            text-primary-text text-sm font-normal font-sans placeholder-primary-text-muted"
                 type="text"
                 required
                 maxlength="30"
@@ -68,8 +68,8 @@
             <input
                 v-model="submissionStore.firstName"
                 data-testid="submit-input-firstname"
-                class="mb-5 px-3 py-3.5 w-[170px] h-[50px] bg-white rounded-lg border border-zinc-400
-            text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
+                class="mb-5 px-3 py-3.5 w-48 h-12 bg-secondary-bg rounded-lg border border-primary-text-muted
+            text-primary-text text-sm font-normal font-sans placeholder-primary-text-muted"
                 type="text"
                 maxlength="30"
                 :placeholder="$t('submitPage.firstName')"
@@ -77,18 +77,19 @@
             >
         </div>
         <span
-            class="mb-2 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
+            class="mb-2 text-primary-text text-sm font-normal font-sans"
         >{{ $t('submitPage.spokenLanguage1') }}</span>
         <p
             v-show="!isValidInput.primarySpokenLangauge.value"
-            class="text-currentColor text-xs font-['Noto Sans JP']"
+            class="text-error text-xs font-sans"
         >
             {{ $t('submitPage.spokenLanguageValidation') }}
         </p>
         <select
             v-model="submissionStore.selectLanguage1"
             data-testid="submit-select-language1"
-            class="mb-5 px-3 py-3.5 w-[350px] h-[50px] bg-white rounded-lg border border-zinc-400 text-primary-text"
+            class="mb-5 px-3 py-3.5 w-96 h-12 bg-secondary-bg rounded-lg border border-primary-text-muted
+                    text-primary-text text-sm font-normal font-sans placeholder-primary-text-muted"
             :placeholder="$t('submitPage.selectLanguage1')"
             @change="initialValidationCheck(submissionStore.selectLanguage1, 'primaryLanguage')"
         >
@@ -102,18 +103,19 @@
             </option>
         </select>
         <span
-            class="mb-2 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
+            class="mb-2 text-primary-text text-sm font-normal font-sans"
         >{{ $t('submitPage.spokenLanguage2') }}</span>
         <p
             v-show="!isValidInput.secondarySpokenLanguage.value"
-            class="text-currentColor text-xs font-['Noto Sans JP']"
+            class="text-error text-xs font-sans"
         >
             {{ $t('submitPage.invalidOption') }}
         </p>
         <select
             v-model="submissionStore.selectLanguage2"
             data-testid="submit-select-language2"
-            class="mb-5 px-3 py-3.5 w-[350px] h-[50px] bg-white rounded-lg border border-zinc-400 text-primary-text"
+            class="mb-5 px-3 py-3.5 w-96 h-12 bg-primary-text-inverted rounded-lg border border-primary-text-muted
+                    text-primary-text text-sm font-normal font-sans placeholder-primary-text-muted"
             :placeholder="$t('submitPage.selectLanguage2')"
             @change="initialValidationCheck(submissionStore.selectLanguage1, 'secondaryLanguage')"
         >
@@ -127,19 +129,19 @@
             </option>
         </select>
         <span
-            class="mb-2 text-neutral-600 text-sm font-normal font-['Noto Sans JP']"
+            class="mb-2 text-primary-text text-sm font-normal font-sans"
         >{{ $t('submitPage.otherNotes') }}({{ $t('submitPage.optional') }})</span>
         <textarea
             v-model="submissionStore.otherNotes"
             data-testid="submit-input-notes"
-            class="mb-5 landscape:mb-[71px] px-3 py-3.5 w-[350px] h-[100px] bg-white rounded-lg border border-zinc-400"
+            class="mb-5 landscape:mb-20 px-3 py-3.5 w-96 h-28 bg-secondary-bg rounded-lg border border-primary-text-muted"
             maxlength="300"
         />
         <button
             data-testid="submit-submitbutton"
             type="submit"
-            class="px-20 py-3 mb-40 landscape:mb-0 rounded-full bg-currentColor w-[350px] text-center
-          text-white text-base font-medium font-['Noto Sans JP']"
+            class="px-20 py-3 mb-40 landscape:mb-0 rounded-full bg-primary w-96 text-center
+             text-primary-inverted font-medium font-sans"
             @click="validateFields"
         >
             {{ $t('submitPage.submitButton') }}
