c84e707bc75401aa2d02707a4d34638318c6b0e2
Dominik Sakic <dominik.sakic@yahoo.com>
2024-07-10 13:53:56 +0900

    feat: create translation into german
    feat: add german to index.ts
    
    chore: change index file german setting, so that the get.json file gets applied
    
    chore: change name of json file to be consisten with language formats

--- i18n/index.ts
+++ i18n/index.ts
@@ -2,6 +2,7 @@ import en from './locales/en.json'
 import ja from './locales/ja.json'
 import pt from './locales/pt.json'
 import ru from './locales/ru.json'
+import de from './locales/de.json'
 
 const i18n = {
     //eslint-disable-next-line
@@ -11,7 +12,9 @@ const i18n = {
     //eslint-disable-next-line
     pt_BR: pt,
     //eslint-disable-next-line
-    ru_RU: ru
+    ru_RU: ru,
+    //eslint-disable-next-line
+    de_DE: de
 }
 
 export default i18n
