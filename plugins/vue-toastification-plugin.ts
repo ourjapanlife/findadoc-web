d0d8ca8708fea706528d878abd3bb43f9b1582e7
NabbeunNabi <williamedwardbrammer@gmail.com>
2024-07-24 11:56:24 +0900

    feat: implement toast plugin for app

---
+++ plugins/vue-toastification-plugin.ts
@@ -0,0 +1,9 @@
+import Toast from 'vue-toastification'
+import { defineNuxtPlugin } from '#app'
+import 'vue-toastification/dist/index.css'
+
+export default defineNuxtPlugin(async nuxtApp => {
+    if (import.meta.client) {
+        nuxtApp.vueApp.use(Toast)
+    }
+})
