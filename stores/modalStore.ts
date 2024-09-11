2e1527b4e4ba9a7d59e0d10d223ad122bcc5095b
ShizuokaTerrier <matt@squareoneJapan.com>
2024-09-11 15:13:05 +0900

    chore: change modalStore into Vue setup store
    Co-authored-by: Christopher Bowman <ProgressChris88@users.noreply.github.com>
    Co-authored-by: NabbeunNabi <williamedwardbrammer@gmail.com>

--- stores/modalStore.ts
+++ stores/modalStore.ts
@@ -1,16 +1,16 @@
 import { defineStore } from 'pinia'
+import { ref, type Ref } from 'vue'
 
-export const useModalStore = defineStore({
-    id: 'modal-store',
-    state: () => ({
-        isOpen: false
-    }),
-    actions: {
-        showModal(): void {
-            this.isOpen = true
-        },
-        hideModal(): void {
-            this.isOpen = false
-        }
+export const useModalStore = defineStore('modal-store', () => {
+    const isOpen: Ref<boolean> = ref(false)
+
+    function showModal(newValue: boolean) {
+        isOpen.value = newValue === true ? true : false
     }
+
+    function hideModal(newValue: boolean) {
+        isOpen.value = newValue === false ? false : true
+    }
+
+    return { isOpen, showModal, hideModal }
 })
