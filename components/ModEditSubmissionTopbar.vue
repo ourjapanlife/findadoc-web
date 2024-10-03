da29256997883046f02687f35a86ba1192dfb6c8
theyokohamalife <lts001@gmail.com>
2024-10-04 00:43:30 +0900

    feat: show modal for rejection confirmation

--- components/ModEditSubmissionTopbar.vue
+++ components/ModEditSubmissionTopbar.vue
@@ -1,5 +1,30 @@
 <template>
     <div class="flex flex-row justify-between w-full">
+        <div
+            v-show="modalStore.isOpen"
+            class="fixed top-0 left-0 flex items-center justify-center h-full w-full z-10 bg-secondary bg-opacity-40"
+        >
+            <Modal
+                class=" min-h-20 min-w-20"
+                @modal-closed="resetModalRefs"
+            >
+                <div
+                    v-if="moderationSubmissionStore.rejectingSubmissionFromTopBar"
+                    class="flex flex-col aspect-square h-96 items-center justify-around bg-primary-inverted p-10 rounded"
+                >
+                    <span class="font-bold text-3xl">
+                        {{ $t('modEditSubmissionTopNav.rejectSubmission') }}
+                    </span>
+                    <button
+                        type="button"
+                        class="bg-primary p-4 rounded-full my-8 font-semibold text-xl"
+                        @click="rejectSubmission"
+                    >
+                        {{ $t('modEditSubmissionTopNav.confirmSubmissionRejection') }}
+                    </button>
+                </div>
+            </Modal>
+        </div>
         <div>
             <button
                 data-testid="mod-edit-submission-copy-submission-id"
@@ -36,7 +61,7 @@
             <button
                 type="button"
                 class="flex justify-center items-center rounded-full bg-secondary-bg border-primary border-2 w-28 text-sm mr-2 "
-                @click="rejectSubmission"
+                @click="confirmRejection"
             >
                 {{
                     $t('modEditSubmissionTopNav.reject') }}
@@ -56,14 +81,18 @@
 <script setup lang="ts">
 import { ref, type Ref } from 'vue'
 import { gql } from 'graphql-request'
+import { useRouter } from 'vue-router'
 import SVGCopyContent from '~/assets/icons/content-copy.svg'
 import SVGSuccessCheckMark from '~/assets/icons/checkmark-square.svg'
 import { useModerationSubmissionsStore } from '~/stores/moderationSubmissionsStore'
 import { useModalStore } from '~/stores/modalStore'
+import { ModerationScreen, useModerationScreenStore } from '~/stores/moderationScreenStore'
 import { gqlClient, graphQLClientRequestWithRetry } from '~/utils/graphql'
 
 const modalStore = useModalStore()
 const moderationSubmissionStore = useModerationSubmissionsStore()
+const screenStore = useModerationScreenStore()
+const router = useRouter()
 const selectedSubmissionId: Ref<string> = ref(moderationSubmissionStore.selectedSubmissionId)
 
 const showCopySuccessIcon: Ref<boolean> = ref(false)
@@ -90,6 +119,8 @@ const acceptSubmission = () => {
 }
 
 const rejectSubmission = async () => {
+    modalStore.hideModal()
+
     const facilityInputVariables = {
         updateSubmissionId: selectedSubmissionId.value,
         input: {
@@ -104,10 +135,25 @@ const rejectSubmission = async () => {
             facilityInputVariables
         )
         moderationSubmissionStore.setDidMutationFail(false)
+        routeUserToModerationScreen()
     } catch (error) {
         console.error('Failed to reject submission:', error)
         moderationSubmissionStore.setDidMutationFail(true)
     }
+}
+
+const routeUserToModerationScreen = () => {
+    screenStore.setActiveScreen(ModerationScreen.Dashboard)
+    router.push('/moderation')
+}
+
+const confirmRejection = async () => {
+    moderationSubmissionStore.setRejectingSubmissionFromTopBar(true)
+    modalStore.showModal()
+}
+
+const resetModalRefs = async () => {
+    moderationSubmissionStore.setRejectingSubmissionFromTopBar(false)
 }
 
 const rejectFacilitySubmissionGqlMutation = gql`
