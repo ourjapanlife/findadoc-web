3b7c9f151a88afa6908b607895011187e9097a11
Gustavo Minetoma <gminetoma@gmail.com>
2024-12-24 21:01:17 +0900

    feat: add lazy load to i18n
    We no longer need the file './i18n.options.ts' to configure i18n.
    
    The configuration is now handled directly in the Nuxt config file.
    
    We are loading the last language used via cookies; therefore, we need to
    disable the language selector and display a loading indicator for the user
    until the loading process is complete.
    
    The browser's language code uses "-", so we need to apply a small
    adjustment to align with our backend typing, which uses "_".
    
    Co-authored-by: Will <williamedwardbrammer@gmail.com>

--- components/LocaleSelector.vue
+++ components/LocaleSelector.vue
@@ -5,30 +5,48 @@
             class="rounded-full landscape:w-full px-2 py-1 landscape:px-1 landscape:py-1.5 border-2 border-primary/80
       landscape:border-primary/60 drop-shadow-md text-primary-text bg-secondary-bg/5
       landscape:bg-primary-bg hover:bg-primary-hover/10 transition-colors"
+            :disabled="!isReady"
         >
             <option
                 v-for="(localeOption) in localeStore.mvpLocaleDisplayOptions"
                 :key="localeOption.code"
                 :value="localeOption.code"
             >
-                {{ localeOption.simpleText }}
+                <span v-if="!isReady">{{ $t("localeSelector.loading") }}</span>
+                <span v-else>{{ localeOption.simpleText }}</span>
             </option>
         </select>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+// Nuxt vue-i18n documentation:
+// https://i18n.nuxtjs.org/docs/v8/api/vue-i18n
+
+import { onMounted, ref, watch } from 'vue'
 import { useLocaleStore } from '../stores/localeStore'
 import { Locale } from '~/typedefs/gqlTypes.js'
 import { useI18n } from '#imports'
 
-const { locale } = useI18n()
+// Displays the loading message for the user. We need to wait until we get the browser cookie to set the current language
+const isReady = ref(false)
+
+//@ts-expect-error - This is a workaround for the fact that the i18n plugin is not typed
+const { setLocale, getLocaleCookie } = useI18n()
 const localeStore = useLocaleStore()
-const selectedLocale = ref(Locale.EnUs)
+
+// getLocaleCookie to set the initial locale, if there is no cookie, it will default based on the nuxt.config.js
+// HACK: Browser's code language is using "-" instead of "_".
+const localeCookie = getLocaleCookie() ? getLocaleCookie().replace('-', '_') : Locale.EnUs
+const selectedLocale = ref(localeCookie)
 
 watch(selectedLocale, (newLocale: Locale) => {
     localeStore.setLocale(newLocale)
-    locale.value = newLocale
+    // setLocale to update the i18n plugin and set the 'i18n_redirected' cookie
+    setLocale(newLocale.replace('_', '-'))
+})
+
+onMounted(() => {
+    isReady.value = true
 })
 </script>
--- i18n.options.ts
+++
@@ -1,9 +0,0 @@
-import i18n from './i18n/index.js'
-
-//@ts-expect-error to ignore the linter
-export default defineI18nConfig(() => ({
-    legacy: false,
-    locale: 'en_US',
-    fallbackLocale: 'en_US',
-    messages: i18n
-}))
