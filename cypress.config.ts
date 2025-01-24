f298af709ec5009b269eaed9f346ae20d0f5475f
Gustavo Minetoma <gminetoma@gmail.com>
2025-01-24 10:20:00 +0900

    feat: add Yarn support for `.env.test`
    - Add Cypress environment variables in `cypress.config`
    - Remove unnecessary support folder configuration
    - Change e2e support file extension to `.ts`
    - Update `.gitignore` to ignore `.env.test`
    
    Added Yarn support for `.env.test` to securely manage environment
    variables required for testing. The `.env.test` file includes:
    - `AUTH0_DOMAIN`
    - `AUTH0_USERNAME`
    - `AUTH0_PASSWORD`
    
    These variables are critical for the Auth0 login process. To ensure
    security, only authorized individuals will have access to the
    `.env.test` file, and it has been added to `.gitignore` to prevent
    accidental commits of sensitive information.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,30 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 import { defineConfig } from 'cypress'
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            AUTH0_DOMAIN: string
+            AUTH0_USERNAME: string
+            AUTH0_PASSWORD: string
+        }
+    }
+}
 
 const config = defineConfig({
     projectId: 'brkojt',
     e2e: {
+        env: {
+            AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+            AUTH0_USERNAME: process.env.AUTH0_USERNAME,
+            AUTH0_PASSWORD: process.env.AUTH0_PASSWORD
+        },
         setupNodeEvents() { },
         // Path to e2e specs folder
         specPattern: './test/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
         // Path to the fake data
         fixturesFolder: './test/fake_data',
-        supportFolder: './test/cypress/support',
-        supportFile: './test/cypress/support/e2e.js',
+        supportFile: './test/cypress/support/e2e.ts',
         baseUrl: 'http://localhost:3000',
         testIsolation: false,
         experimentalRunAllSpecs: true,
