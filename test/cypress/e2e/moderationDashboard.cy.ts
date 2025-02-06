3138b91619cd6ea42bcf3a0b0df6e9c5ab1cd22e
theyokohamalife <lts001@gmail.com>
2025-02-06 21:47:36 +0900

    feat: show modal to confirm submission rejection

--- test/cypress/e2e/moderationDashboard.cy.ts
+++ test/cypress/e2e/moderationDashboard.cy.ts
@@ -107,3 +107,66 @@ describe('Moderation dashboard', () => {
         })
     })
 })
+
+describe('Moderation Reject Submission', () => {
+    before(function() {
+        cy.fixture(FAKE_SUBMISSION_RESPONSE_PATH).then(fakeSubmissionResponse => {
+            this.fakeSubmissionResponse = fakeSubmissionResponse
+        })
+    })
+
+    context('Landscape mode', () => {
+        before(function() {
+            cy.viewport('macbook-16')
+
+            cy.visit('/login', { timeout: 10000 })
+            Cypress.session.clearCurrentSessionData()
+
+            // This intercepts the call to the graphQL api in order to use fake data in the tests to protect
+            // the real data.
+            cy.intercept('POST', '**/', req => {
+                if (req.body.query && req.body.query.includes('query Submissions')) {
+                    req.reply({
+                        statusCode: 200,
+                        body: this.fakeSubmissionResponse
+                    })
+                } else {
+                    req.continue()
+                }
+            }).as('getSubmissions')
+
+            cy.origin('https://findadoc.jp.auth0.com/', () => {
+                cy.get('input#username').should('be.visible').type('findadoctest@proton.me')
+                cy.get('[data-action-button-primary]').should('be.visible').click()
+                cy.get('input#password').should('be.visible').type('vCnL5J8agHg6m2f')
+                cy.get('[data-action-button-primary]').should('be.visible').click()
+            })
+
+            cy.url({ timeout: 10000 }).should('equal', 'http://localhost:3000/')
+
+            cy.get('[data-testid=top-nav-mod-link]', { timeout: 10000 }).click().visit('/moderation')
+
+            cy.url({ timeout: 10000 }).should('include', '/moderation')
+
+            cy.wait('@getSubmissions', { timeout: 10000 })
+
+            cy.get('[data-testid="mod-submission-list-item-1"]', { timeout: 10000 }).click()
+        })
+
+        after(() => {
+            // Session data is cleared again so it does not affect following tests.
+            Cypress.session.clearCurrentSessionData()
+        })
+
+        it('should display confirmation modal if user clicks the reject button', () => {
+            // When the user clicks the reject button for a submission...
+            cy.get('[data-testid="mod-edit-submission-reject-button"]').click()
+            // ...the modal with the confirmation button should be visible.
+            cy.get('[data-testid="reject-confirmation"]').should('be.visible')
+            // When the user clicks the confirmation button on the modal...
+            cy.get('[data-testid="reject-submission-confirmation-btn"]').click()
+            // ...they should be navigated back to the moderation screen.
+            cy.location('pathname').should('eq', '/moderation')
+        })
+    })
+})
