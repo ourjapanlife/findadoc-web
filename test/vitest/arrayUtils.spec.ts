6478eac9a30af83bf9bd53459865ef7f58d8b222
vororo98 <47042311+vororo98@users.noreply.github.com>
2025-02-11 17:21:09 +0100

    non-primitive data type implemented in  arraysAreEqual
    arraysAreEqual now can check if an array containing objects or arrays are equal. It does this by turning the values into strings. Also added tests for the function in a new file.

---
+++ test/vitest/arrayUtils.spec.ts
@@ -0,0 +1,46 @@
+import { setActivePinia, createPinia } from 'pinia'
+import { expect } from 'chai'
+import { arraysAreEqual } from '@/utils/arrayUtils'
+
+describe('arraysAreEqual', () => {
+    beforeEach(() => {
+        setActivePinia(createPinia())
+    })
+
+    it('should sort and find two arrays with the same primitive values to be equal', () => {
+        expect(arraysAreEqual([1,2,3], [3,1,2])).to.be.true;
+    })
+
+    it('should sort and find two arrays with different primitive values to be not equal', () => {
+        expect(arraysAreEqual([1,2,3], [4,5,6])).to.be.false;
+    })
+
+
+    it('should sort and find two arrays with the same object values to be equal', () => {
+        const arr1 = [{name: "Tony", profession: "tester"}, {name: "Sara", profession: "coder"}];
+        const arr2 = [{name: "Sara", profession: "coder"}, {name: "Tony", profession: "tester"}];
+
+        expect(arraysAreEqual(arr1, arr2)).to.be.true;
+    })
+
+    it('should sort and find two arrays with different object values to be not equal', () => {
+        const arr1 = [{name: "Tony", profession: "tester"}, {name: "Sara", profession: "coder"}];
+        const arr2 = [{name: "Max", profession: "Truck Driver"}, {name: "Helen", profession: "History Professor"}];
+
+        expect(arraysAreEqual(arr1, arr2)).to.be.false;
+    })
+
+    it('should sort and find two arrays with the same array values to be equal', () => {
+        const arr1 = [[1,2,3], [4,5,6]];
+        const arr2 = [ [4,5,6], [1,2,3]];
+
+        expect(arraysAreEqual(arr1, arr2)).to.be.true;
+    })
+
+    it('should sort and find two arrays with different array values to be not equal', () => {
+        const arr1 = [[1,2,3], [4,5,6]];
+        const arr2 = [ [7,8,9], [1,2,3]];
+
+        expect(arraysAreEqual(arr1, arr2)).to.be.false;
+    })
+})
--- utils/arrayUtils.ts
+++ utils/arrayUtils.ts
@@ -8,7 +8,7 @@ export function removeDuplicates<T>(inputArray: T[]): T[] {
     }, [])
 }
 
-//This is a helper function that sorts and compares that all the values in a primitive value array are equal
+//This is a helper function that sorts and compares that all the values in an array are equal
 export function arraysAreEqual<T>(array1: T[],
     array2: T[]): boolean {
     if (array1.length !== array2.length) {
@@ -16,9 +16,41 @@ export function arraysAreEqual<T>(array1: T[],
     }
 
     // Sort both arrays
-    const sortedArray1 = [...array1].sort((a, b) => (a > b ? 1 : -1))
-    const sortedArray2 = [...array2].sort((a, b) => (a > b ? 1 : -1))
+    const sortedArray1 = [...array1].sort(compareElements)
+    const sortedArray2 = [...array2].sort(compareElements)
 
     // Compare each element in the sorted arrays
-    return sortedArray1.every((value, index) => value === sortedArray2[index])
+    for (let i = 0; i < sortedArray1.length; i++) {
+        if (canonicalize(sortedArray1[i]) !== canonicalize(sortedArray2[i])) {
+          return false;
+        }
+      }
+
+      return true;
+}
+
+function compareElements<T>(ele1: T, ele2: T): number {
+    const  ele1Str = canonicalize(ele1);
+    const  ele2Str = canonicalize(ele2);
+    if (ele1Str < ele2Str) return -1;
+    if (ele1Str > ele2Str) return 1;
+    return 0
+}
+
+//This function handles different data types and returns them as canonicalized strings
+function canonicalize(value: any): string {
+    if (value === null) return 'null';
+    if (value === undefined) return 'undefined';
+    
+    if (typeof value !== 'object') {
+      return JSON.stringify(value);
+    }
+    
+    if (Array.isArray(value)) {
+      return '[' + value.map(canonicalize).join(',') + ']';
+    }
+    
+    const keys = Object.keys(value).sort();
+    const keyValuePairs = keys.map(key => `${JSON.stringify(key)}:${canonicalize(value[key])}`);
+    return '{' + keyValuePairs.join(',') + '}';
 }
