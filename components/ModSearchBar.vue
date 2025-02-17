a9b6b36342aeb27997f0323df772fda0560ab093
prestonb91 <prestonb91@gmail.com>
2025-02-17 22:16:40 +0900

    fix: remove extra semicolon

--- components/ModSearchBar.vue
+++ components/ModSearchBar.vue
@@ -167,8 +167,8 @@ const handleListItem = () => {
     const item = filteredItems.value[selectedItemIndex.value]
 
     if (selectedItems.value.includes(item)) {
-        const itemIndex = selectedItems.value.indexOf(item);
-        selectedItems.value.splice(itemIndex, 1);
+        const itemIndex = selectedItems.value.indexOf(item)
+        selectedItems.value.splice(itemIndex, 1)
         return
     }
 
