4f3d1ac69d2b5ce33d7e32c0df14b0797b0d45c4
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-06-15 00:10:44 +0900

    toast test

--- components/ModCreateHealthcareProfessionalSection.vue
+++ components/ModCreateHealthcareProfessionalSection.vue
@@ -267,8 +267,8 @@
 </template>
 
 <script lang="ts" setup>
-import { onBeforeMount, reactive, type Ref, ref, watch } from 'vue'
-import { type ToastInterface, useToast } from 'vue-toastification'
+import { reactive, type Ref, ref, watch } from 'vue'
+import { useToast } from 'vue-toastification'
 import ModSearchbar from './ModSearchBar.vue'
 import { useHealthcareProfessionalsStore } from '~/stores/healthcareProfessionalsStore'
 import { useFacilitiesStore } from '~/stores/facilitiesStore'
@@ -283,7 +283,7 @@ import { Insurance,
     type HealthcareProfessional } from '~/typedefs/gqlTypes'
 import { useI18n } from '#imports'
 
-let toast: ToastInterface
+toast = useToast()
 
 const { t } = useI18n()
 
@@ -558,15 +558,6 @@ const specialtiesToDisplayCallback = (specialty: Specialty) => [specialty]
 const degreesToDisplayCallback = (degree: Degree) => [degree]
 const insurancesToDisplayCallback = (insurance: Insurance) => [insurance]
 const localesToDisplayCallback = (locale: Locale) => [localesStore.formatLanguageCodeToSimpleText(locale)]
-
-onBeforeMount(async () => {
-    /**
-    Set the variable to useToast when the before the component mounts
-    since vue-taostification is only available on the client.
-    If not done this way the build fails
-     */
-    toast = useToast()
-})
 
 // This updates the array in the store once it is updated due to a facility clicked
 watch(selectedFacilities.value, newValue => {
