8216cd1653b9deb90146af06a3c677402c358061
ermish <philipermish@gmail.com>
2025-06-18 01:00:24 +0900

    updated specialties available and added top 10

--- stores/specialtiesStore.ts
+++ stores/specialtiesStore.ts
@@ -1,35 +1,67 @@
 import { defineStore } from 'pinia'
+import { useI18n } from 'vue-i18n'
 import type { Specialty } from '~/typedefs/gqlTypes.js'
 
-export const useSpecialtiesStore = defineStore('specialtiesStore', () => ({ specialtyDisplayOptions }))
+export const useSpecialtiesStore = defineStore('specialtiesStore', () => {
+    const { t } = useI18n()
+
+    const topSpecialties = [
+        { code: 'DENTISTRY', displayText: t('specialties.dentistry') },
+        { code: 'DERMATOLOGY', displayText: t('specialties.dermatology') },
+        { code: 'PEDIATRICS', displayText: t('specialties.pediatrics') },
+        { code: 'OBSTETRICS_AND_GYNECOLOGY', displayText: t('specialties.obstetricsAndGynecology') },
+        { code: 'GENERAL_PRACTITIONER', displayText: t('specialties.generalPractitioner') },
+        { code: 'PSYCHIATRY', displayText: t('specialties.psychiatry') },
+        { code: 'PHYSIOTHERAPY', displayText: t('specialties.physiotherapy') },
+        { code: 'OPTOMETRY', displayText: t('specialties.optometry') },
+        { code: 'ENT_SPECIALIST', displayText: t('specialties.entSpecialist') },
+        { code: 'EMERGENCY_MEDICINE', displayText: t('specialties.emergencyMedicine') },
+    ]
+
+    const specialtyDisplayOptions = [
+        { code: '', displayText: '----Any----' },
+        { code: 'ALLERGY_AND_IMMUNOLOGY', displayText: t('specialties.allergyAndImmunology') },
+        { code: 'ANESTHESIOLOGY', displayText: t('specialties.anesthesiology') },
+        { code: 'CARDIOLOGY', displayText: t('specialties.cardiology') },
+        { code: 'DENTISTRY', displayText: t('specialties.dentistry') },
+        { code: 'DERMATOLOGY', displayText: t('specialties.dermatology') },
+        { code: 'DIAGNOSTIC_RADIOLOGY', displayText: t('specialties.diagnosticRadiology') },
+        { code: 'EMERGENCY_MEDICINE', displayText: t('specialties.emergencyMedicine') },
+        { code: 'ENT_SPECIALIST', displayText: t('specialties.entSpecialist') },
+        { code: 'INFECTIOUS_DISEASES', displayText: t('specialties.infectiousDiseases') },
+        { code: 'GENERAL_PRACTITIONER', displayText: t('specialties.generalPractitioner') },
+        { code: 'MEDICAL_GENETICS', displayText: t('specialties.medicalGenetics') },
+        { code: 'NEUROLOGY', displayText: t('specialties.neurology') },
+        { code: 'NUCLEAR_MEDICINE', displayText: t('specialties.nuclearMedicine') },
+        { code: 'OBSTETRICS_AND_GYNECOLOGY', displayText: t('specialties.obstetricsAndGynecology') },
+        { code: 'OPHTHALMOLOGY', displayText: t('specialties.ophthalmology') },
+        { code: 'OPTOMETRY', displayText: t('specialties.optometry') },
+        { code: 'ORTHOPEDIC_SURGERY', displayText: t('specialties.orthopedicSurgery') },
+        { code: 'PATHOLOGY', displayText: t('specialties.pathology') },
+        { code: 'PEDIATRICS', displayText: t('specialties.pediatrics') },
+        { code: 'PHYSIOTHERAPY', displayText: t('specialties.physiotherapy') },
+        { code: 'PSYCHIATRY', displayText: t('specialties.psychiatry') },
+        { code: 'RADIATION_ONCOLOGY', displayText: t('specialties.radiationOncology') },
+        { code: 'SPORTS_MEDICINE', displayText: t('specialties.sportsMedicine') },
+        { code: 'UROLOGY', displayText: t('specialties.urology') }
+        // Intentionally excluded specialties
+        // { code: 'PREVENTIVE_MEDICINE', displayText: t('specialties.preventiveMedicine') },
+        // { code: 'SURGERY', displayText: t('specialties.surgery') },
+        // { code: 'TRAUMATOLOGY', displayText: t('specialties traumatology') },
+        // Combined into General Practitioner / Pediatrition since most people don't know the nuanced difference
+        // { code: 'INTERNAL_MEDICINE', displayText: t('specialties.internalMedicine') },
+        // { code: 'FAMILY_MEDICINE', displayText: t('specialties.familyMedicine') },
+        // Combined with Sports Medicine
+        // { code: 'PHYSICAL_MEDICINE_AND_REHABILITATION', displayText: t('specialties.physicalMedicineAndRehabilitation') },
+    ] as SpecialtyDisplayOption[]
+
+    return {
+        topSpecialties,
+        specialtyDisplayOptions
+    }
+})
 
 export interface SpecialtyDisplayOption {
     code: Specialty
     displayText: string
 }
-
-const specialtyDisplayOptions = [
-    { code: '', displayText: '----Any----' },
-    { code: 'ALLERGY_AND_IMMUNOLOGY', displayText: 'Allergy and Immunology' },
-    { code: 'ANESTHESIOLOGY', displayText: 'Anesthesiology' },
-    { code: 'DENTISTRY', displayText: 'Dentistry' },
-    { code: 'DERMATOLOGY', displayText: 'Dermatology' },
-    { code: 'DIAGNOSTIC_RADIOLOGY', displayText: 'Diagnostic Radiology' },
-    { code: 'EMERGENCY_MEDICINE', displayText: 'Emergency Medicine' },
-    { code: 'FAMILY_MEDICINE', displayText: 'Family Medicine' },
-    { code: 'INTERNAL_MEDICINE', displayText: 'Internal Medicine' },
-    { code: 'INFECTIOUS_DISEASES', displayText: 'Infectious Diseases' },
-    { code: 'GENERAL_MEDICINE', displayText: 'General Medicine' },
-    { code: 'MEDICAL_GENETICS', displayText: 'Medical Genetics' },
-    { code: 'NEUROLOGY', displayText: 'Neurology' },
-    { code: 'NUCLEAR_MEDICINE', displayText: 'Nuclear Medicine' },
-    { code: 'OBSTETRICS_AND_GYNECOLOGY', displayText: 'Obstetrics and Gynecology' },
-    { code: 'OPHTHALMOLOGY', displayText: 'Ophthalmology' },
-    { code: 'PATHOLOGY', displayText: 'Pathology' },
-    { code: 'PEDIATRICS', displayText: 'Pediatrics' },
-    { code: 'PHYSICAL_MEDICINE_AND_REHABILITATION', displayText: 'Physical Medicine and Rehabilitation' },
-    { code: 'PREVENTIVE_MEDICINE', displayText: 'Preventive Medicine' },
-    { code: 'PSYCHIATRY', displayText: 'Psychiatry' },
-    { code: 'RADIATION_ONCOLOGY', displayText: 'Radiation Oncology' },
-    { code: 'SURGERY', displayText: 'Surgery' },
-    { code: 'UROLOGY', displayText: 'Urology' }] as SpecialtyDisplayOption[]
