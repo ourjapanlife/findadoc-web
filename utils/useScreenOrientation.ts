19188e906c8584b8ed2e039d39b6d44e6aeddac4
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-06-21 00:23:29 +0900

    refactor: update for new screen sizes

--- utils/useScreenOrientation.ts
+++ utils/useScreenOrientation.ts
@@ -2,19 +2,26 @@ import { ref, computed, onMounted, onUnmounted } from 'vue'
 
 export const useScreenOrientation = () => {
     const width = ref(window.innerWidth)
+    const height = ref(window.innerHeight)
 
     const updateWidth = () => {
         width.value = window.innerWidth
     }
 
+    const updateHeight = () => {
+        height.value = window.innerHeight
+    }
+
     onMounted(() => {
         window.addEventListener('resize', updateWidth)
+        window.addEventListener('resize', updateHeight)
     })
     onUnmounted(() => {
         window.removeEventListener('resize', updateWidth)
+        window.removeEventListener('resize', updateHeight)
     })
 
-    const isPortrait = computed(() => width.value <= 768)
+    const isPortrait = computed(() => width.value <= height.value)
 
     return { isPortrait }
 }
