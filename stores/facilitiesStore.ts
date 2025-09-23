d23c3f081217912d7e52813e0c148fbceda669cf
Lisa Broersen <lj.broersen@gmail.com>
2025-09-23 13:14:33 +0900

    chore: anotherr linter error fix that the script did not fix by itself

--- stores/facilitiesStore.ts
+++ stores/facilitiesStore.ts
@@ -14,7 +14,6 @@ import type {
     ContactInput,
     PhysicalAddress,
     PhysicalAddressInput,
-    CreateFacilityInput,
     UpdateFacilityInput,
     MutationCreateFacilityArgs
 } from '~/typedefs/gqlTypes'
