25460873e9f000f14fbaf12c40b8e27ad5d2cab3
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-12-28 09:58:30 +0900

    fix: keep topnav and footer always visible
    This updates the layout to use h-dvh with a flex column structure where only the main content scrolls. This avoids the 100vh (h-screen) mobile viewport issues and prevents the header and footer from disappearing mid-page. I applied shrink-0 to the TopNav and Footer so they keep their natural height and don't change based on the viewheight. The main content area uses flex-1 to fill the remaining space.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,21 @@
 <template>
-    <!-- The main app should be set to h-screen. This is to ensure that the TopNav and Footer are
-        always visible on desktop screens without scrolling.
-    -->
-    <div
-        data-testid="app"
-        class="h-screen w-full font-sans text-primary-text bg-primary-bg"
-    >
-        <TopNav />
-        <NuxtPage />
-        <Footer />
+    <!--
+    The main app should se h-dvh (dynamic viewport height) so the app fits the visible viewport on mobile
+    which helps us avoid any 100vh / h-screen issue where the browser address bars change the viewport and
+    can cause jumpy layouts or inconsistencies with fixed positioning.
+  - We often add overflow-y-auto to the main area so the main app should have a
+    flex column layout where only the <main> area scrolls. This helps keep the
+    TopNav and Footer always visible without needing position.
+  - The TopNav and Footer should use shrink-0 they don’t shrink when the middle content grows;
+    the main content gets the remaining space via flex-1.
+-->
+    <div class="h-dvh w-full font-sans text-primary-text bg-primary-bg flex flex-col">
+        <TopNav class="shrink-0" />
+
+        <main class="flex-1 overflow-y-auto">
+            <NuxtPage />
+        </main>
+
+        <Footer class="shrink-0" />
     </div>
 </template>
-
-<script setup lang="ts">
-</script>
