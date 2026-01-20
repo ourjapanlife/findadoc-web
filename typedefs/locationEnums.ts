3155c41a8fc3286dbdaa91fbde13bc875f17e87b
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-01-20 17:07:22 +0900

    enums for region

---
+++ typedefs/locationEnums.ts
@@ -0,0 +1,15 @@
+export enum Region {
+    Hokkaido = 'HOKKAIDO',
+    Tohoku = 'TOHOKU',
+    Kanto = 'KANTO',
+    Chubu = 'CHUBU',
+    Kansai = 'KANSAI',
+    Chugoku = 'CHUGOKU',
+    Shikoku = 'SHIKOKU',
+    Kyushu = 'KYUSHU',
+    Okinawa = 'OKINAWA'
+}
+
+export enum Prefecture {
+
+}
