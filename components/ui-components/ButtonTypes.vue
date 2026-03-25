875bde272e678462f5ef5555c283574b3626c270
Andrew Leemhuis <tanukilandslide@gmail.com>
2026-03-25 13:03:16 +0900

    Made code dryer

--- components/ui-components/ButtonTypes.vue
+++ components/ui-components/ButtonTypes.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="buttonTypes[buttonStyle]">
+    <div :class="`${buttonTypes[props.buttonStyle]} ${baseStyles}`">
         <slot />
     </div>
 </template>
@@ -7,15 +7,16 @@
 <script setup>
 const props = defineProps({
     buttonStyle: {
-        type: String
+        type: String,
+        required: true
     }
 })
 
-const buttonStyle = props.buttonStyle
+const baseStyles = 'inline w-fit h-fit px-6 py-3 flex justify-center items-center rounded-full cursor-pointer'
 
 const buttonTypes = {
-    primary: 'bg-primary hover:bg-primary-hover active:opacity-50 text-white inline w-fit h-fit px-6 py-3 flex justify-center items-center rounded-full cursor-pointer',
-    primaryOutline: 'border-primary hover:bg-primary-hover hover:text-white active:opacity-50 text-primary border-2 w-fit h-fit px-6 py-3 flex justify-center items-center rounded-full cursor-pointer',
-    destructive: 'bg-error hover:opacity-50 w-fit h-fit px-6 py-3 text-white flex justify-center items-center rounded-full cursor-pointer'
+    primary: 'bg-primary hover:bg-primary-hover active:opacity-50 text-white',
+    primaryOutline: 'border-primary hover:bg-primary-hover hover:text-white active:opacity-50 text-primary border-2',
+    destructive: 'bg-error hover:opacity-50 active:opacity-75 text-white'
 }
 </script>
