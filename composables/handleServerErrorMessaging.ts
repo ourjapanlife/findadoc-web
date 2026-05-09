c2c77c2c89f94c4b9fea44676987bc0c405fe153
BobbyRobBob <imbobby10@gmail.com>
2026-05-09 16:50:59 +0800

    fix: Prevent submission page from showing success message when submission fails #1428 (#1725)

--- composables/handleServerErrorMessaging.ts
+++ composables/handleServerErrorMessaging.ts
@@ -5,6 +5,10 @@ export function handleServerErrorMessaging(
     serverErrors: ServerError[], toast: ToastInterface,
     t: (translatableString: string) => string | undefined
 ) {
+    if (serverErrors.length === 0) {
+        toast.error(t('serverErrorMessages.genericErrorMessage') ?? 'An error occurred')
+        return
+    }
     serverErrors.forEach(error => {
         if (t(`serverErrorMessages.${error.code}`)) {
             toast.error((t(`serverErrorMessages.${error.code}`)))
