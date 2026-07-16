b6be0abf83b6d58d838dcbc69dbd289a9570f610
Jackson Merle <jackson.merle@gmail.com>
2026-07-15 22:37:57 -0700

    feat: snapshot test is setup to track submissions tab of moderation

--- tests/vitest/snapshotTests/moderation.spec.ts
+++ tests/vitest/snapshotTests/moderation.spec.ts
@@ -1,10 +1,12 @@
 import { describe, expect, it, vi } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { flushPromises, mount } from '@vue/test-utils'
 import { createI18n } from 'vue-i18n'
 import Moderation from '~/pages/moderation.vue'
 
 const mockAuthStore = {
     isTestingMode: true,
+    isLoggedIn: true,
+    isLoadingAuth: false,
     redirectIfUnauthenticatedUser: vi.fn()
 }
 
@@ -13,7 +15,13 @@ vi.mock('~/stores/authStore', () => ({
 }))
 
 vi.mock('vue-router', () => ({
-    useRoute: () => useRoute()
+    useRoute: () => ({
+        path: '/moderation',
+        params: {},
+        query: {
+            view: 'submissions'
+        }
+    })
 }))
 
 const i18n = createI18n({
@@ -23,13 +31,16 @@ const i18n = createI18n({
 })
 
 describe('moderation.vue (vitest snapshot)', () => {
-    it('renders correctly', () => {
+    it('renders correctly', async () => {
         const wrapper = mount(Moderation, {
             global: {
-                plugins: [i18n]
+                plugins: [i18n],
+                stubs: {
+                    ModLeftNavbar: true
+                }
             }
         })
-
+        await flushPromises()
         expect(wrapper.html()).toMatchSnapshot()
     })
 })
